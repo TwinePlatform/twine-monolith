e7eaa6200bcc55d989084aac0bf9e27676df7443
Danny <danny.connell@reasondigital.com>
2016-08-18 16:43:21 +0100

    Got charts working on dashboard. Got date field on log hours page populating with today's date. Other visual tweaks.

--- volunteer-app/www/js/app.js
+++ volunteer-app/www/js/app.js
@@ -12,12 +12,12 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.styleBlackTranslucent();
     }
   });
 })
