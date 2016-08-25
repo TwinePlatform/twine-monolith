0e34994deb36e688aaff1101dccc422912aa7194
Danny Connell <danny.connell@readingroom.com>
2016-08-25 08:27:14 +0100

    Started setting up location reminders functionality

--- www/js/controllers/settings-controller.js
+++ www/js/controllers/settings-controller.js
@@ -10,8 +10,81 @@
 	> settings controller
 */
 
-	angular.module('app').controller('SettingsController', ['$scope', '$stateParams', '$http',
-	function ($scope, $stateParams, $http) {
+	angular.module('app').controller('SettingsController', ['$scope', '$stateParams', '$http', '$ionicPopup', '$ionicLoading',
+	function ($scope, $stateParams, $http, $ionicPopup, $ionicLoading) {
+
+		/*
+			>> location reminders
+		*/
+
+			$scope.locationReminders = function() {
+
+				var $this = this;
+
+				// switch turned on
+				if ($this.locationRemindersSwitch) { // FIX THIS ?
+
+					// ask user if they're in the location where they volunteer
+					var locationRemindersPopup = $ionicPopup.show({
+						template: 'Are you in the location where you volunteer right now?',
+						title: 'Setup location reminders',
+						scope: $scope,
+						buttons: [
+							{
+								text: 'No',
+								onTap: function(e) {
+									// user tapped no, tell user to try again when at correct location
+									var tryAgainLaterPopup = $ionicPopup.alert({
+									  	title: 'Please try later',
+									  	template: 'Please setup location reminders later, when you are in your volunteering location.'
+									});
+									tryAgainLaterPopup.then(function(res) {
+									  	// disable switch
+									  	$this.locationRemindersSwitch = false; // FIX THIS ?
+									});
+								}
+							},
+							{
+							  	text: '<b>Yes</b>',
+							  	type: 'button-positive',
+							  	onTap: function(e) {
+							    	// user tapped yes, setup location reminders
+							    	$scope.enableLocationReminders();
+							  	}
+							}
+						]
+					});
+
+				}
+				// switch turned off
+				else {
+					// disable location reminders
+					$scope.disableLocationReminders();
+				}
+			}
+
+
+		/*
+			>> enable location reminders
+		*/
+
+			$scope.enableLocationReminders = function() {
+		    	console.log('enable location reminders');
+		    	$ionicLoading.show();
+		    	setTimeout(function(){
+		    		$ionicLoading.hide();
+		    	}, 1000);
+			}
+
+
+		/*
+			>> disable location reminders
+		*/
+
+			$scope.disableLocationReminders = function() {
+		    	console.log('disable location reminders');
+			}
+
 
 		/*
 			>> populate organisation dropdown
