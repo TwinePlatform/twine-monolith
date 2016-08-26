1979beba562b4195aef9732b9509467380684f38
Danny Connell <danny.connell@readingroom.com>
2016-08-26 16:43:09 +0200

    Added shout notifications. Got log hours page working

--- volunteer-app/www/js/controllers/log-hours-controller.js
+++ volunteer-app/www/js/controllers/log-hours-controller.js
@@ -2,22 +2,167 @@
 * CONTENTS
 *
 * log hours controller
-*   get today's date
+*   store the form data
+*   function: generate form date
+*   get today's date for raw date field
+*   populate organisation dropdown
+*    loop through the results and push only required items to $scope.organisations
+*   process the form
+*    validate form
+*    submit form
+*   process connection error
 */
 
 /*
 	> log hours controller
 */
 
-	angular.module('app').controller('LogHoursController', ['$scope', '$stateParams',
-	function ($scope, $stateParams) {
+	angular.module('app').controller('LogHoursController', ['$scope', '$stateParams', '$http', '$filter', '$ionicLoading', 
+	function ($scope, $stateParams, $http, $filter, $ionicLoading) {
 
 		/*
-			>> get today's date
+			>> store the form data
+		*/
+
+			$scope.formData = {};
+
+
+		/*
+			>> function: generate form date
 		*/
 		
+			// generate a date in the format 2017-08-30 and add to hidden date field
+			$scope.generateFormDate = function() {
+				var formDate = $filter('date')($scope.formData.dateRaw, 'yyyy-MM-dd');
+				$scope.formData.date = formDate;
+			}
+
+
+		/*
+			>> get today's date for raw date field
+		*/
+		
+			// get today's date
 			$scope.today = new Date();
 
-			console.log($scope.today);
+			// set the raw date field to today
+			$scope.formData.dateRaw = $scope.today;
+
+			// generate the form date
+			$scope.generateFormDate();
+
+
+		/*
+			>> populate organisation dropdown
+		*/
+
+			$scope.organisations = [];
+			$http({
+				method: 'GET',
+				url: api('organisations')
+			}).success(function (result) {
+				// console.log(result.data);
+				$scope.organisations = result.data;
+				// >>> loop through the results and push only required items to $scope.organisations
+				for (var i = 0, len = result.data.length; i < len; i++) {
+					$scope.organisations[i] = {id: result.data[i].id, name: result.data[i].name};
+				}
+			}).error(function (result, error) {
+				
+				// process connection error
+				$scope.processConnectionError(result, error);
+
+			});
+
+		/*
+			>> process the form
+		*/
+
+			$scope.formSubmitted = false;
+			$scope.processForm = function(form) {
+
+				// >>> validate form
+
+				// variable to show that form was submitted
+				$scope.formSubmitted = true;
+
+				// form is valid
+				if (form.$valid) {
+					console.log('form valid');
+
+					// console.log($scope.formData);
+					// console.log($.param($scope.formData));
+
+					// show loader
+					$ionicLoading.show();
+
+					// >>> submit form
+					$http({
+						method: 'POST',
+						url: api('logs'),
+						data: $.param($scope.formData),
+						headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+					}).success(function(response) {
+
+						console.log(response);
+
+						// create log successful
+						if (response.success) {
+
+							console.log('create log successful');
+
+							setTimeout(function(){
+
+								// hide loader
+								$ionicLoading.hide();
+
+								// shout success
+								shout('Log saved succesfully!');
+
+							}, 200);
+
+						}
+
+						// create log unsuccessful
+						else {
+
+							console.log('create log unsuccessful');
+
+						}
+
+					}).error(function(data, error) {
+
+						// process connection error
+						$scope.processConnectionError(data, error);
+
+					});
+				}
+				// form is invalid
+				else {
+					console.log('form invalid');
+				}
+
+			};
+
+		/*
+			>> process connection error
+		*/
+
+			$scope.processConnectionError = function(data, error) {
+
+				// no internet connection
+				if (error === 0) {
+					$scope.connectionError = 'Could not connect. Please check your internet connection & try again.';
+				}
+				// resource not found
+				else if (error === 404) {
+					$scope.connectionError = 'Error: the requested resource was not found.';
+				}
+				// other error
+				else {
+					$scope.connectionError = 'Could not connect. Please try again later. Error code: ' + error;
+				}
+
+			}
 
 	}])
--- volunteer-app/www/js/global-variables.js
+++ volunteer-app/www/js/global-variables.js
@@ -9,3 +9,48 @@
 	var api = function(url) {
 		return apiBaseUrl + url;
 	}
+
+	// shout - show a quick, non-intrusive popup message to the user
+	// e.g. shout('<p>Hello!</p>', 1500);
+	var shout = function(content, pause) {
+		if (typeof pause == 'undefined') {pause = 1500}
+
+		// remove any existing shout
+		$('.shout').remove();
+
+		// element variables				
+		var randomNumber = Math.floor((Math.random() * 999) + 1);
+		$('<div class="shout hidden shout-' + randomNumber + '"></div>').appendTo('body');
+		$shout = $('.shout-' + randomNumber);
+		$('<div class="shout-content"></div>').appendTo($('.shout-' + randomNumber));
+		$shoutContent = $('.shout-' + randomNumber + ' .shout-content');
+
+		// setup shout
+		$('.shout-' + randomNumber).removeClass('hidden');
+		$('.shout-' + randomNumber + ' .shout-content').css({
+			transform: 'scale(2)',
+			opacity: 0
+		}).html(content);
+
+		// show shout
+		setTimeout(function(){
+			$('.shout-' + randomNumber + ' .shout-content').css({
+				opacity: 1,
+				transform: 'scale(1)'
+			});
+		}, 300);
+
+		// hide shout
+		setTimeout(function(){
+			$('.shout-' + randomNumber + ' .shout-content').css({
+				opacity: 0,
+				transform: 'scale(3)',			
+			});
+		}, 600 + pause);
+
+		// fully hide shout
+		setTimeout(function(){
+			$('.shout-' + randomNumber).addClass('hidden');
+		}, 600 + pause + 300);
+
+	} 
