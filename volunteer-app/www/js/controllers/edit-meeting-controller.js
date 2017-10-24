a1712a7b1e20dd0338d52fb618f3164be2ca2e46
Yurii Firs <yurii@wetelo.com>
2017-10-24 16:10:02 +0300

    Meetings

--- volunteer-app/www/js/controllers/edit-meeting-controller.js
+++ volunteer-app/www/js/controllers/edit-meeting-controller.js
@@ -21,10 +21,8 @@
 	> edit meeting controller
 */
 
-	angular.module('app.controllers').controller('EditMeetingController', function (
-		$scope, $stateParams, $state, $http, $ionicLoading, $filter, $localStorage, $rootScope, $timeout,
-		$$api, $$utilities, $$shout, $$offline
-	) {
+angular.module('app.controllers').controller('EditMeetingController', function ($scope, $stateParams, $state, $http, $ionicLoading, $filter, $localStorage, $rootScope, $timeout,
+                                                                                $$api, $$utilities, $$shout, $$offline) {
 
     /*
 				>> populate meetingTypes dropdown
@@ -34,250 +32,249 @@
     $scope.meetingTypes = [];
 
 
+    /*
+        >> variables
+    */
 
-		/*
-			>> variables
-		*/
-
-			$scope.formData = {};
-			$scope.meetingLoaded = false;
-
+    $scope.formData = {};
+    $scope.meetingLoaded = false;
 
 
-		/*
-			>> setup datepickers
-		*/
+    /*
+        >> setup datepickers
+    */
 
-			var $datepickerInput = $('.editLog .datepicker').pickadate({
-				min: $$utilities.getDateFirstOfMonth(),
-				container: '.datepicker-container',
-				clear: false,
-				onSet: function(context) {
-					// add date to scope in correct format
-					$scope.formData.date = $filter('date')(context.select, 'yyyy-MM-dd');
+    var $datepickerInput = $('.editLog .datepicker').pickadate({
+        min: $$utilities.getDateFirstOfMonth(),
+        container: '.datepicker-container',
+        clear: false,
+        onSet: function (context) {
+            // add date to scope in correct format
+            $scope.formData.date = $filter('date')(context.select, 'yyyy-MM-dd');
 
-					// add current time
-					$scope.formData.date = $scope.formData.date + ' ' + $$utilities.getCurrentTimeAsString();
-				}
-			});
-
-
-		/*
-			>> show loader
-		*/
-
-			$ionicLoading.show();
+            // add current time
+            $scope.formData.date = $scope.formData.date + ' ' + $$utilities.getCurrentTimeAsString();
+        }
+    });
 
 
+    /*
+        >> show loader
+    */
 
-		/*
-			>> display meeting data
-		*/
+    $ionicLoading.show();
 
-			$scope.displayMeetingData = function(mainResult) {
 
-				console.log('result: ', mainResult);
+    /*
+        >> display meeting data
+    */
 
-				// set datepicker date
-				var picker = $datepickerInput.pickadate('picker');
-				var dateShort = mainResult.data.date.substring(0,10);
-				picker.set('select', dateShort, { format: 'yyyy-mm-dd' } );
+    $scope.displayMeetingData = function (mainResult) {
+
+        console.log('result: ', mainResult);
+
+        // set datepicker date
+        var picker = $datepickerInput.pickadate('picker');
+        var dateShort = mainResult.data.date.substring(0, 10);
+        picker.set('select', dateShort, {format: 'yyyy-mm-dd'});
         $$api.meetingTypes.get().success(function (result) {
-          $scope.formData = mainResult.data;
-          // loop through the results and push only required items to $scope.meetingTypes
-          for (var i = 0, len = result.data.length; i < len; i++) {
-            $scope.meetingTypes[i] = {id: result.data[i].id, name: result.data[i].name};
-            if ($scope.formData.type===result.data[i].id) {
-              $scope.formData.type = result.data[i];
+            $scope.formData = mainResult.data;
+            // loop through the results and push only required items to $scope.meetingTypes
+            for (var i = 0, len = result.data.length; i < len; i++) {
+                $scope.meetingTypes[i] = {id: result.data[i].id, name: result.data[i].name};
+                if ($scope.formData.type === result.data[i].id) {
+                    $scope.formData.type = result.data[i];
+                }
             }
-          }
-          // enable meetingTypes select
-          $scope.meetingTypesDisabled = false;
+            // enable meetingTypes select
+            $scope.meetingTypesDisabled = false;
 
-          // add to scope
-          $scope.formData = mainResult.data;
-          // get the offline_id for this meeting from $localStorage
-          var offlineMeeting =  $.grep($localStorage.offlineData.meetings, function(e){ return e.id === $scope.formData.id; });
-          $scope.formData.offline_id = offlineMeeting[0].offline_id;
+            // add to scope
+            $scope.formData = mainResult.data;
+            // get the offline_id for this meeting from $localStorage
+            var offlineMeeting = $.grep($localStorage.offlineData.meetings, function (e) {
+                return e.id === $scope.formData.id;
+            });
+            $scope.formData.offline_id = offlineMeeting[0].offline_id;
 
-          // meeting data has finished loading
-          $scope.meetingLoaded = true;
+            // meeting data has finished loading
+            $scope.meetingLoaded = true;
 
-          // hide loader
-          $ionicLoading.hide();
+            // hide loader
+            $ionicLoading.hide();
         }).error(function (result, error) {
 
-          // process connection error
-          $$utilities.processConnectionError(result, error);
+            // process connection error
+            $$utilities.processConnectionError(result, error);
 
         });
 
-			}
+    }
 
 
-		/*
-			>> get meeting data
-		*/
+    /*
+        >> get meeting data
+    */
 
-			// if offline mode, edit offline meeting
-			if ($rootScope.offlineMode) {
+    // if offline mode, edit offline meeting
+    if ($rootScope.offlineMode) {
 
-				var result = $$offline.getMeeting($state.params.offline_id);
+        var result = $$offline.getMeeting($state.params.offline_id);
 
-				setTimeout(function(){
-					$scope.displayMeetingData(result);
-				}, 100);
+        setTimeout(function () {
+            $scope.displayMeetingData(result);
+        }, 100);
 
-			}
+    }
 
-			// else get meeting from api
-			else {
+    // else get meeting from api
+    else {
 
-				// get meeting id
-				$scope.meetingId = $state.params.id;
+        // get meeting id
+        $scope.meetingId = $state.params.id;
 
-				// get meeting from api
-				$$api.meetings.getMeeting($scope.meetingId).success(function (result) {
+        // get meeting from api
+        $$api.meetings.getMeeting($scope.meetingId).success(function (result) {
 
-					// set initial duration (used for making sure user can't meeting more than 24 hours)
-					if (angular.isUndefined($scope.initialDuration)) {
-						$scope.initialDuration = result.data.duration;
-					}
+            // set initial duration (used for making sure user can't meeting more than 24 hours)
+            if (angular.isUndefined($scope.initialDuration)) {
+                $scope.initialDuration = result.data.duration;
+            }
 
-					$scope.displayMeetingData(result);
+            $scope.displayMeetingData(result);
 
-				}).error(function (result, error) {
+        }).error(function (result, error) {
 
-					// couldn't connect, send back to view meetings and enable offline mode
-					$state.go('tabs.view-logs.meetings');
+            // couldn't connect, send back to view meetings and enable offline mode
+            $state.go('tabs.view-logs.meetings');
 
-					// process connection error
-					$$utilities.processConnectionError(result, error);
+            // process connection error
+            $$utilities.processConnectionError(result, error);
 
-				});
+        });
 
-			}
+    }
 
 
-		/*
-			>> function: generate form date
-		*/
+    /*
+        >> function: generate form date
+    */
 
-			// generate a date in the format 2017-08-30 and add to hidden date field
-			$scope.generateFormDate = function() {
-				var formDate = $filter('date')($scope.formData.dateRaw, 'yyyy-MM-dd');
-				$scope.formData.date = formDate;
-			}
+    // generate a date in the format 2017-08-30 and add to hidden date field
+    $scope.generateFormDate = function () {
+        var formDate = $filter('date')($scope.formData.dateRaw, 'yyyy-MM-dd');
+        $scope.formData.date = formDate;
+    }
 
-		/*
-			>> process the edit meeting form
-		*/
+    /*
+        >> process the edit meeting form
+    */
 
-			console.log('$$offline.checkConnection(): ', $$offline.checkConnection());
+    console.log('$$offline.checkConnection(): ', $$offline.checkConnection());
 
-			$scope.formSubmitted = false;
-			$scope.processForm = function(form) {
+    $scope.formSubmitted = false;
+    $scope.processForm = function (form) {
 
-				// >>> validate form
+        // >>> validate form
 
-				// variable to show that form was submitted
-				$scope.formSubmitted = true;
+        // variable to show that form was submitted
+        $scope.formSubmitted = true;
 
-				// form is valid
-				if (form.$valid) {
+        // form is valid
+        if (form.$valid) {
 
-					console.log('$scope.formData: ', $scope.formData);
+            console.log('$scope.formData: ', $scope.formData);
 
-					// show loader
-					$ionicLoading.show();
+            // show loader
+            $ionicLoading.show();
 
-					// if no internet connection, switch to offline mode
-					if (!$$offline.checkConnection()) {
-						$$offline.enable();
-					}
+            // if no internet connection, switch to offline mode
+            if (!$$offline.checkConnection()) {
+                $$offline.enable();
+            }
 
-					// if offline mode, save to offline data and marks as needs_pushing
-					if ($rootScope.offlineMode) {
+            // if offline mode, save to offline data and marks as needs_pushing
+            if ($rootScope.offlineMode) {
 
-						// save offline
-						$$offline.editMeeting({ id: $scope.formData.offline_id, idKey: 'offline_id' }, $scope.formData, true);
+                // save offline
+                $$offline.editMeeting({id: $scope.formData.offline_id, idKey: 'offline_id'}, $scope.formData, true);
 
-						// hide loader
-						$ionicLoading.hide();
+                // hide loader
+                $ionicLoading.hide();
 
-						// go back to view meetings
-						$state.go('tabs.view-logs.meetings');
+                // go back to view meetings
+                $state.go('tabs.view-logs.meetings');
 
-						// shout success
-						$$shout('Meeting saved locally!');
+                // shout success
+                $$shout('Meeting saved locally!');
 
-					}
+            }
 
-					// else save to api
-					else {
+            // else save to api
+            else {
 
-						// >>> submit edit meeting form
-						$$api.meetings.edit($scope.formData.id, $.param($scope.formData)).success(function (result) {
+                // >>> submit edit meeting form
+                $$api.meetings.edit($scope.formData.id, $.param($scope.formData)).success(function (result) {
 
-							console.log($scope.formData);
+                    console.log($scope.formData);
 
-							console.log('result: ', result);
+                    console.log('result: ', result);
 
-							// create meeting successful
-							if (result.success) {
+                    // create meeting successful
+                    if (result.success) {
 
-								// hide loader
-								$ionicLoading.hide();
+                        // hide loader
+                        $ionicLoading.hide();
 
-								// save offline
-								$$offline.editMeeting({ id: $scope.formData.id, idKey: 'id' }, result.data);
+                        // save offline
+                        $$offline.editMeeting({id: $scope.formData.id, idKey: 'id'}, result.data);
 
-								// go back to view meetings
-								$state.go('tabs.view-logs.meetings');
+                        // go back to view meetings
+                        $state.go('tabs.view-logs.meetings');
 
-								// shout success
-								$$shout('Meeting saved succesfully!');
+                        // shout success
+                        $$shout('Meeting saved succesfully!');
 
-							}
+                    }
 
-							// create meeting unsuccessful
-							else {
+                    // create meeting unsuccessful
+                    else {
 
-								$$shout('Edit meeting unsuccessful');
+                        $$shout('Edit meeting unsuccessful');
 
-							}
+                    }
 
-						}).error(function(result, error) {
+                }).error(function (result, error) {
 
-							// if user does not exist, meeting user out
-							if (error === 404) {
-								$$utilities.logOut('This user account no longer exists.');
-							}
-							else {
+                    // if user does not exist, meeting user out
+                    if (error === 404) {
+                        $$utilities.logOut('This user account no longer exists.');
+                    }
+                    else {
 
-								$$shout('Could not edit meeting. Please try again in offline mode.');
+                        $$shout('Could not edit meeting. Please try again in offline mode.');
 
-								// enable offline mode
-								$$offline.enable();
+                        // enable offline mode
+                        $$offline.enable();
 
-								// go back to view meetings
-								$state.go('tabs.view-logs.meetings');
+                        // go back to view meetings
+                        $state.go('tabs.view-logs.meetings');
 
-								// process connection error
-								$$utilities.processConnectionError(data, error);
+                        // process connection error
+                        $$utilities.processConnectionError(data, error);
 
-							}
+                    }
 
-						});
+                });
 
-					}
+            }
 
-				}
-				// form is invalid
-				else {
-					console.log('form invalid!!');
-				}
+        }
+        // form is invalid
+        else {
+            console.log('form invalid!!');
+        }
 
-			};
+    };
 
-	})
+})
--- volunteer-app/www/js/routes.js
+++ volunteer-app/www/js/routes.js
@@ -14,182 +14,205 @@
 
 angular.module('app.routes', ['ionicUIRouter'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-	$stateProvider
+        $stateProvider
 
-		// > register page
-		.state('register', {
-			cache: false,
-			url: '/register',
-			templateUrl: 'templates/register.html',
-			controller: 'RegisterController'
-		})
+        // > register page
+            .state('register', {
+                cache: false,
+                url: '/register',
+                templateUrl: 'templates/register.html',
+                controller: 'RegisterController'
+            })
 
-		// > login page
-		.state('login', {
-			url: '/login',
-			templateUrl: 'templates/login.html',
-			controller: 'LoginController'
-		})
+            // > login page
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: 'LoginController'
+            })
 
-		// > tabs
-		.state('tabs', {
-			url: '/tab',
-			abstract: true,
-			templateUrl: 'templates/tabs.html'
-		})
+            // > tabs
+            .state('tabs', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
 
-			// >> dashboard
-			.state('tabs.dashboard', {
-				url: '/dashboard',
-				views: {
-					'dashboard-tab': {
-						templateUrl: 'templates/dashboard.html',
-						controller: 'DashboardController'
-					}
-				}
-			})
+            // >> dashboard
+            .state('tabs.dashboard', {
+                url: '/dashboard',
+                views: {
+                    'dashboard-tab': {
+                        templateUrl: 'templates/dashboard.html',
+                        controller: 'DashboardController'
+                    }
+                }
+            })
 
-				// >>> dashboard - new log
-				.state('tabs.new-log', {
-					cache: false,
-					url: '/dashboard/new-log',
-					views: {
-						'dashboard-tab': {
-							templateUrl: 'templates/new-log.html',
-							controller: 'NewLogController'
-						}
-					}
-				})
+            // >>> dashboard - new log
+            .state('tabs.new-log', {
+                cache: false,
+                url: '/dashboard/new-log',
+                views: {
+                    'dashboard-tab': {
+                        templateUrl: 'templates/new-log.html',
+                        controller: 'NewLogController'
+                    }
+                }
+            })
 
-			// >> view logs
-			.state('tabs.view-logs', {
-				cache: false,
-				url: '/view-logs',
-				views: {
-				  'view-logs-tab': {
-					templateUrl: 'templates/view-logs.html'
-				  }
-				}
-			})
-      	// >> view logs - hours
-				.state('tabs.view-logs.hours', {
-					cache: false,
-					url: '/hours',
-					views: {
-						'view-logs-hours-tab@tabs.view-logs': {
-							templateUrl: 'templates/view-logs-hours.html',
-							controller: 'ViewLogsHoursController'
-						}
-					}
-				})
+            // >> view logs
+            .state('tabs.view-logs', {
+                cache: false,
+                url: '/view-logs',
+                views: {
+                    'view-logs-tab': {
+                        templateUrl: 'templates/view-logs.html'
+                    }
+                }
+            })
+            // >> view logs - hours
+            .state('tabs.view-logs.hours', {
+                cache: false,
+                url: '/hours',
+                views: {
+                    'view-logs-hours-tab@tabs.view-logs': {
+                        templateUrl: 'templates/view-logs-hours.html',
+                        controller: 'ViewLogsHoursController'
+                    }
+                }
+            })
 
-				// >> view logs - meetings
-				.state('tabs.view-logs.meetings', {
-					cache: false,
-					url: '/meetings',
-					views: {
-						'view-logs-meetings-tab@tabs.view-logs': {
-							templateUrl: 'templates/view-logs-meetings.html',
-							controller: 'ViewLogsMeetingsController'
-						}
-					}
-				})
-				// >>> view logs - meetings - new log
-				.state('tabs.new-meeting-log', {
-					cache: false,
-					url: '/new-meeting-log',
-					views: {
-						'view-logs-meetings-tab@tabs.view-logs': {
-							templateUrl: 'templates/new-meeting-log.html',
-							controller: 'NewMeetingLogController'
-						}
-					}
-				})
-				// >>> view logs - meetings - new log
-				.state('tabs.edit-meeting', {
-					cache: false,
-					url: '/edit-meeting/:id',
-					views: {
-						'view-logs-meetings-tab@tabs.view-logs': {
-							templateUrl: 'templates/edit-meeting.html',
-							controller: 'EditMeetingController'
-						}
-					}
-				})
+            // >> view logs - meetings
+            .state('tabs.view-logs.meetings', {
+                cache: false,
+                url: '/meetings',
+                views: {
+                    'view-logs-meetings-tab@tabs.view-logs': {
+                        templateUrl: 'templates/view-logs-meetings.html',
+                        controller: 'ViewLogsMeetingsController'
+                    }
+                }
+            })
+            // >>> view logs - meetings - new log
+            .state('tabs.new-meeting-log', {
+                cache: false,
+                url: '/new-meeting-log',
+                views: {
+                    'view-logs-meetings-tab@tabs.view-logs': {
+                        templateUrl: 'templates/new-meeting-log.html',
+                        controller: 'NewMeetingLogController'
+                    }
+                }
+            })
+            // >>> view logs - meetings - new log
+            .state('tabs.edit-meeting', {
+                cache: false,
+                url: '/edit-meeting/:id',
+                views: {
+                    'view-logs-meetings-tab@tabs.view-logs': {
+                        templateUrl: 'templates/edit-meeting.html',
+                        controller: 'EditMeetingController'
+                    }
+                }
+            })
 
-				// >>> view logs - edit log
-				.state('tabs.edit-log', {
-					cache: false,
-					url: '/edit-log/:id',
-					views: {
-						'view-logs-hours-tab@tabs.view-logs': {
-							templateUrl: 'templates/edit-log.html',
-							controller: 'EditLogController'
-						}
-					}
-				})
+            // >>> view logs - edit log
+            .state('tabs.edit-log', {
+                cache: false,
+                url: '/edit-log/:id',
+                views: {
+                    'view-logs-hours-tab@tabs.view-logs': {
+                        templateUrl: 'templates/edit-log.html',
+                        controller: 'EditLogController'
+                    }
+                }
+            })
 
-				// >>> view logs - edit log offline
-				.state('tabs.edit-log-offline', {
-					cache: false,
-					url: '/edit-log-offline/:offline_id',
-					views: {
-						'view-logs-tab': {
-							templateUrl: 'templates/edit-log.html',
-							controller: 'EditLogController'
-						}
-					}
-				})
+            // >>> view logs - edit log offline
+            .state('tabs.edit-log-offline', {
+                cache: false,
+                url: '/edit-log-offline/:offline_id',
+                views: {
+                    'view-logs-tab': {
+                        templateUrl: 'templates/edit-log.html',
+                        controller: 'EditLogController'
+                    }
+                }
+            })
 
-        // >> volunteers
-				.state('tabs.view-volunteers', {
-					// cache: 'false',
-					url: '/volunteers',
-					views: {
-						'view-volunteers-tab': {
-							templateUrl: 'templates/view-volunteers.html',
-							controller: 'ViewVolunteersController'
-						}
-					}
-				})
-			// >> settings
-			.state('tabs.settings', {
-				// cache: 'false',
-				url: '/settings',
-				views: {
-					'settings-tab': {
-						templateUrl: 'templates/settings.html',
-						controller: 'SettingsController'
-					}
-				}
-			})
+            // >> volunteers
+            .state('tabs.view-volunteers', {
+                // cache: 'false',
+                url: '/volunteers',
+                views: {
+                    'view-volunteers-tab': {
+                        templateUrl: 'templates/view-volunteers.html',
+                        controller: 'ViewVolunteersController'
+                    }
+                }
+            })
+            // > new volunteer
+            .state('tabs.new-volunteer', {
+                cache: false,
+                url: '/new-volunteer',
+                views: {
+                    'view-volunteers-tab': {
+                        templateUrl: 'templates/new-volunteer.html',
+                        controller: 'NewVolunteerController'
+                    }
+                }
+            })
 
-				// >>> settings - profile
-				.state('tabs.profile', {
-					url: '/profile',
-					views: {
-						'settings-tab': {
-							templateUrl: 'templates/profile.html',
-							controller: 'ProfileController'
-						}
-					}
-				})
+            // >>> edit volunteer
+            .state('tabs.edit-volunteer', {
+                cache: false,
+                url: '/edit-volunteer/:id',
+                views: {
+                    'view-volunteers-tab': {
+                        templateUrl: 'templates/edit-volunteer.html',
+                        controller: 'EditVolunteerController'
+                    }
+                }
+            })
 
-				// >>> settings - terms & conditions
-				.state('tabs.terms-and-conditions', {
-					url: '/terms-and-conditions',
-					views: {
-						'settings-tab': {
-							templateUrl: 'templates/terms-and-conditions.html'
-						}
-					}
-				})
+            // >> settings
+            .state('tabs.settings', {
+                // cache: 'false',
+                url: '/settings',
+                views: {
+                    'settings-tab': {
+                        templateUrl: 'templates/settings.html',
+                        controller: 'SettingsController'
+                    }
+                }
+            })
+
+            // >>> settings - profile
+            .state('tabs.profile', {
+                url: '/profile',
+                views: {
+                    'settings-tab': {
+                        templateUrl: 'templates/profile.html',
+                        controller: 'ProfileController'
+                    }
+                }
+            })
+
+            // >>> settings - terms & conditions
+            .state('tabs.terms-and-conditions', {
+                url: '/terms-and-conditions',
+                views: {
+                    'settings-tab': {
+                        templateUrl: 'templates/terms-and-conditions.html'
+                    }
+                }
+            })
 
 
+        // default page
+        $urlRouterProvider.otherwise('/register');
 
-		// default page
-		$urlRouterProvider.otherwise('/register');
-
-});
+    });
