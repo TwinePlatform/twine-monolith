796b4b1f472415480070f5159657e76e5dcb75b9
Elias Malik <elias0789@gmail.com>
2018-06-07 23:06:44 +0100

    🔧 setup common linter config
    Related to #1

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "node": true,
+    "jest": true
+  },
+  "rules": {
+    "semi": [ "error", "always" ],
+    "brace-style": [ "error", "1tbs" ],
+    "padded-blocks": "off",
+    "comma-dangle": [ "error", "always-multiline" ],
+    "arrow-parens": [ "error", "always" ],
+    "consistent-return": "off",
+    "no-confusing-arrow": "off",
+    "no-shadow": "off",
+    "no-prototype-builtins": "off",
+    "camelcase": "off",
+  }
+};
