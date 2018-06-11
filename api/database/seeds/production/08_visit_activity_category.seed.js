4435d0d817344254ffa98a115c137effba2af127
Elias Malik <elias0789@gmail.com>
2018-06-11 12:40:33 +0100

    🗃 replace sql seed files with knex seed files
    This is again (see #10) to avoid having to write
    our own cli scripts. Also, have separate seeds for
    development, testing and production environments
    (although currently they're identical)

---
+++ api/database/seeds/production/08_visit_activity_category.seed.js
@@ -0,0 +1,22 @@
+exports.seed = (knex) =>
+  knex('visit_activity_category')
+    .truncate()
+    .insert([
+      { visit_activity_category_name: 'Sports' },
+      { visit_activity_category_name: 'Arts, Craft, and Music' },
+      { visit_activity_category_name: 'Physical health and wellbeing' },
+      { visit_activity_category_name: 'Socialising' },
+      { visit_activity_category_name: 'Adult skills building' },
+      { visit_activity_category_name: 'Education support' },
+      { visit_activity_category_name: 'Employment support' },
+      { visit_activity_category_name: 'Business support' },
+      { visit_activity_category_name: 'Care service' },
+      { visit_activity_category_name: 'Mental health support' },
+      { visit_activity_category_name: 'Housing support' },
+      { visit_activity_category_name: 'Work space' },
+      { visit_activity_category_name: 'Food' },
+      { visit_activity_category_name: 'Outdoor work and gardening' },
+      { visit_activity_category_name: 'Local products' },
+      { visit_activity_category_name: 'Environment and conservation work' },
+      { visit_activity_category_name: 'Transport' },
+    ]);
---
+++ api/database/seeds/testing/06_outreach_campaign_valid_target.seed.js
@@ -0,0 +1,63 @@
+exports.seed = (knex) => {
+  const getTypeIdByName = (name) =>
+    knex('outreach_type')
+      .select('outreach_type_id')
+      .where({ outreach_type_name: name });
+
+  const getTargetIdByName = (name) =>
+    knex('outreach_campaign_target_type')
+      .select('outreach_campaign_target_type_id')
+      .where({ outreach_campaign_target_name: name });
+
+  return knex('outreach_campaign_target')
+    .truncate()
+    .insert([
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Community Group') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Resident') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Investor') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Local business') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Local professional forum') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('Local media') },
+      { outreach_type_id: getTypeIdByName('Community Share Issue'), outreach_campaign_target_type_id: getTargetIdByName('National media') },
+
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Community Group') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Resident') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Investor') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Local business') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Local professional forum') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Local media') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('National media') },
+      { outreach_type_id: getTypeIdByName('Crowdfunding (donations)'), outreach_campaign_target_type_id: getTargetIdByName('Larger Corporate') },
+
+      { outreach_type_id: getTypeIdByName('Asset Transfer'), outreach_campaign_target_type_id: getTargetIdByName('Local business') },
+      { outreach_type_id: getTypeIdByName('Asset Transfer'), outreach_campaign_target_type_id: getTargetIdByName('Larger Corporate') },
+
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Community Group') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Resident') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Investor') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local business') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local media') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('National media') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – elected member') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – officer – asset management') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – officer – other') },
+
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Community Group') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Resident') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Investor') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local media') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('National media') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Corporate') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – officer') },
+      { outreach_type_id: getTypeIdByName('Community Energy Project'), outreach_campaign_target_type_id: getTargetIdByName('Energy Provider') },
+
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Community Group') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Resident') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Investor') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Local media') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('National media') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Corporate') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – elected member') },
+      { outreach_type_id: getTypeIdByName('Public Services Commissioning'), outreach_campaign_target_type_id: getTargetIdByName('Local authority – officer') },
+    ]);
+};
