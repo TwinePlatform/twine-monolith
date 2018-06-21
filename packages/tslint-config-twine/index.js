7485553baa01c08f9fd0931485e7947ae625b058
AstroAsh <astroaisha@gmail.com>
2018-06-21 12:42:02 +0100

    💡 Update rules to tslint flavours

--- packages/tslint-config-twine/index.js
+++ packages/tslint-config-twine/index.js
@@ -1,19 +1,9 @@
 module.exports = {
   "extends": "tslint-config-airbnb",
-  "env": {
-    "node": true,
-    "jest": true
-  },
   "rules": {
-    "semi": [ "error", "always" ],
-    "brace-style": [ "error", "1tbs" ],
-    "padded-blocks": "off",
-    "comma-dangle": [ "error", "always-multiline" ],
-    "arrow-parens": [ "error", "always" ],
-    "consistent-return": "off",
-    "no-confusing-arrow": "off",
-    "no-shadow": "off",
-    "no-prototype-builtins": "off",
-    "camelcase": "off",
-  }
+    "semicolon": [ true, "always" ],
+    "trailing-comma": [true, {"multiline": "always", "singleline": "never"}],
+    "arrow-parens": true,
+    "no-shadowed-variable": false
+    }
 };
