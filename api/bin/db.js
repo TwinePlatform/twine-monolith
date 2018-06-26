18d5058be7565d3346abbfd75089e9b385046ae9
Elias Malik <elias0789@gmail.com>
2018-06-26 11:39:48 +0100

    🗃 add teardown and reset scripts for the DB
    Not using "down" migrations so need a way to reset the DB.

--- api/bin/db.js
+++ api/bin/db.js
@@ -2,8 +2,19 @@
 const parse = require('minimist');
 const db = require('../database');
 
+
+process.on('unhandledRejection', (err) => { throw err });
+
 const { _: args } = parse(process.argv.slice(2));
 
 const [mod, command] = args[0].split(':');
 
-db[mod][command](...args.slice(1));
+(async () => {
+  try {
+    await db[mod][command](...args.slice(1));
+    console.log('Done!');
+  } catch (error) {
+    console.error('Something went wrong!');
+    console.error(error);
+  }
+})();
--- api/database/index.js
+++ api/database/index.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const { compose, last, head } = require('ramda');
+const knex = require('knex');
+const { compose, last, head, tap } = require('ramda');
+const { getConfig } = require('../config');
+
+// lazyPromiseSeries :: [PromiseLike (a)] -> Promise ([a])
+const lazyPromiseSeries = (ps) => ps.reduce((acc, p) => p.then((res) => [].concat(acc ? acc : []).concat(res)))
 
 const MIGRATIONS_BASE_PATH = path.resolve(__dirname, 'migrations');
 
@@ -35,11 +40,12 @@ const templates = {
     'const { buildQueryFromFile } = require(\'..\');',
     'exports.up = buildQueryFromFile(__filename);',
     'exports.down = () => {};',
+    '',
   ].join('\n'),
 };
 
 exports.migrate = {
-  make: (name) => {
+  make: async (name) => {
     const date = (new Date()).toISOString().slice(0, 10).replace(/-/g, '');
     const files = fs.readdirSync(path.resolve(__dirname, 'migrations', 'sql')).sort();
     const newVersion = Number(last(files).split('_')[1]) + 1;
@@ -47,5 +53,41 @@ exports.migrate = {
 
     write(path.resolve(MIGRATIONS_BASE_PATH, 'sql', `${newFilename}.sql`), templates.sql);
     write(path.resolve(MIGRATIONS_BASE_PATH, `${newFilename}.js`), templates.js);
+
+    return 1;
+  },
+
+  teardown: async (name, env = process.env.NODE_ENV) => {
+    const config = getConfig(env);
+
+    const client = knex(config.knex);
+
+    const tables = await client('pg_catalog.pg_tables').select('tablename').where({ schemaname: 'public' });
+
+    const queries = tables
+      .map((t) => t.tablename)
+      .filter((t) => t !== 'spatial_ref_sys')
+      .map(tap((t) => console.log(`Dropping table ${t}`)))
+      .map((tablename) => client.raw(`DROP TABLE IF EXISTS "${tablename}" CASCADE`))
+      .concat([
+        'ENUM_gender',
+        'ENUM_turnover_band',
+        'ENUM_permission_type',
+        'ENUM_permission_access',
+        'ENUM_invitation_status',
+        'ENUM_organisation_type',
+        'ENUM_subscription_status',
+      ]
+        .map(tap((t) => console.log(`Dropping type ${t}`)))
+        .map((e) => client.raw(`DROP TYPE IF EXISTS ${e} CASCADE`))
+      );
+
+    await client.transaction((trx) =>
+      lazyPromiseSeries(queries.map((q) => q.transacting(trx)))
+        .then(trx.commit)
+        .catch(trx.rollback)
+    )
+
+    return client.destroy();
   },
 };
