1483fcdfb51e1ce995d49c8eea16bc1cb783439d
Elias Malik <elias0789@gmail.com>
2018-07-10 16:46:04 +0100

    🗜 add internal typings file
    which I missed somehow 😬

---
+++ api/src/types/internal.d.ts
@@ -0,0 +1,27 @@
+export type Nothing = null;
+export type Maybe<T> = T | Nothing;
+
+export type Map<K extends string | number, V> = {
+  [k in K]: V
+}
+
+export type Dictionary<T> = {
+  [key: string]: T
+}
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>
+}
+
+export type JsonPrimitives =
+  string
+  | number
+  | boolean
+  | null
+
+export type JsonTypes =
+  JsonPrimitives
+  | Dictionary<JsonPrimitives>
+  | JsonPrimitives[]
+
+export type Json = Dictionary<JsonTypes> | Dictionary<JsonTypes>[] | JsonTypes[]
