b45989863083df5ec0832975be89e93d524dd851
Elias Malik <elias0789@gmail.com>
2018-07-17 18:26:17 +0100

    ✨ add renameKeys function again

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,3 +1,10 @@
+import { Dictionary, CurriedFunction2, assoc, curry } from 'ramda';
+
+
+type RenameKeys<T extends Dictionary<any>, L extends Dictionary<string>> =
+  CurriedFunction2<L, T, { [k in L[keyof L]]: any }> ;
+
+
 export const lazyPromiseSeries = (ps: PromiseLike<any>[]): Promise<any[]> =>
   Promise.resolve()
     .then(() =>
@@ -6,3 +13,10 @@ export const lazyPromiseSeries = (ps: PromiseLike<any>[]): Promise<any[]> =>
         Promise.resolve([])
       )
     );
+
+
+export const renameKeys: RenameKeys<Dictionary<any>, Dictionary<string>> =
+  curry((map, o) =>
+    Object.keys(o)
+      .reduce((acc, k) => assoc(map[k] || k, o[k], acc), {})
+);
