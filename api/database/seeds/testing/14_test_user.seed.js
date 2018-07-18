11ff466b007456afdbf4e342c12f48828c95c47e
Elias Malik <elias0789@gmail.com>
2018-07-18 13:03:31 +0100

    🗃 add second user to test db seed

--- api/database/seeds/testing/14_test_user.seed.js
+++ api/database/seeds/testing/14_test_user.seed.js
@@ -16,5 +16,21 @@ exports.seed = (knex) =>
         gender_id: knex('gender').select('gender_id').where({ gender_name: 'female' }),
         disability_id: knex('disability').select('disability_id').where({ disability_name: 'no' }),
         ethnicity_id: knex('ethnicity').select('ethnicity_id').where({ ethnicity_name: 'prefer not to say' }),
+      },
+      {
+        user_name: 'Gordon',
+        user_password: 'tobereplacedwhenauthissetup',
+        email: '1998@blackmesaresearch.com',
+        phone_number: '+1 555-555-3141',
+        post_code: '82394',
+        birth_year: 1974,
+        is_email_confirmed: false,
+        is_phone_number_confirmed: false,
+        is_email_contact_consent_granted: false,
+        is_sms_contact_consent_granted: false,
+        deleted_at: '2018-07-07T15:14:22Z+01',
+        gender_id: knex('gender').select('gender_id').where({ gender_name: 'male' }),
+        disability_id: knex('disability').select('disability_id').where({ disability_name: 'no' }),
+        ethnicity_id: knex('ethnicity').select('ethnicity_id').where({ ethnicity_name: 'prefer not to say' }),
       }
     ]);
