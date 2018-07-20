a29e3a08be9fd03bc2f9ad8db781bd442ac16240
AstroAsh <astroaisha@gmail.com>
2018-07-20 08:26:41 -0400

    👌🏽 Insert test seed data based on queries

--- api/database/seeds/testing/16_test_user_role.seed.js
+++ api/database/seeds/testing/16_test_user_role.seed.js
@@ -2,13 +2,13 @@ exports.seed = (knex) =>
   knex('user_account_access_role')
     .insert([
       { 
-        user_account_id: 1,
-        organisation_id: 1,
-        access_role_id: 1,
+        user_account_id: knex('user_account').select('user_account_id').where({ user_name: 'Chell' }),
+        organisation_id: knex('organisation').select('organisation_id').where({ organisation_name: 'Aperture Science' }),
+        access_role_id: knex('access_role').select('access_role_id').where({ access_role_name: 'VISITOR' }),
       },
       { 
-        user_account_id: 2,
-        organisation_id: 1,
-        access_role_id: 5,
+        user_account_id: knex('user_account').select('user_account_id').where({ user_name: 'GlaDos' }),
+        organisation_id: knex('organisation').select('organisation_id').where({ organisation_name: 'Aperture Science' }),
+        access_role_id: knex('access_role').select('access_role_id').where({ access_role_name: 'ORG_ADMIN' }),
       },
   ]);
