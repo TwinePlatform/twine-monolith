27bec26e764152da25cbe95694be22c1f3bf2267
Elias Malik <elias0789@gmail.com>
2018-07-23 22:49:06 +0100

    🏭 add model factories and seed data for testing (+1 squashed commit) Squashed commits: [d961f66] user role

--- api/database/seeds/testing/16_test_user_role.seed.js
+++ api/database/seeds/testing/16_test_user_role.seed.js
@@ -11,4 +11,9 @@ exports.seed = (knex) =>
         organisation_id: knex('organisation').select('organisation_id').where({ organisation_name: 'Aperture Science' }),
         access_role_id: knex('access_role').select('access_role_id').where({ access_role_name: 'ORG_ADMIN' }),
       },
+      {
+        user_account_id: knex('user_account').select('user_account_id').where({ user_name: 'Gordon' }),
+        organisation_id: knex('organisation').select('organisation_id'). where({ organisation_name: 'Aperture Science' }),
+        access_role_id: knex('access_role').select('access_role_id').where({ access_role_name: 'ORG_ADMIN' }),
+      }
   ]);
--- api/tests/factory.ts
+++ api/tests/factory.ts
@@ -6,9 +6,39 @@ factory.define('user', Object, {
   email: factory.chance('email'),
 });
 
-factory.define('organisation', Object, {
+factory.extend('user', 'cbAdmin', {
+  isEmailConfirmed: factory.chance('bool'),
+});
+
+factory.extend('user', 'visitor', {
   name: factory.chance('name'),
+  email: factory.chance('email'),
+  phoneNumber: factory.chance('phone', { country: 'uk' }),
+  qrCode: factory.chance('string'),
+  gender: factory.chance('pickone', ['male', 'female', 'prefer not to say']),
+  disability: factory.chance('pickone', ['yes', 'no', 'prefer not to say']),
+  ethnicity: factory.chance('pickone', ['prefer not to say']),
+  birthYear: factory.chance('integer', { min: 1900, max: 2018 }),
+  postCode: factory.chance('postal'),
+  isEmailConfirmed: factory.chance('bool'),
+  isPhoneNumberConfirmed: factory.chance('bool'),
+  isEmailConsentGranted: factory.chance('bool'),
+  isSMSConsentGranted: factory.chance('bool'),
+});
+
+factory.define('organisation', Object, {
+  name: factory.chance('word'),
   _360GivingId: factory.chance('string'),
+});
+
+factory.extend('organisation', 'communityBusiness', {
+  region: factory.chance('pickone', ['London', 'Yorkshire and the Humber', 'North East']),
+  sector: factory.chance('pickone', ['Art centre or facility', 'Energy', 'Housing', 'Transport']),
+  logoUrl: factory.chance('url'),
+  address1: factory.chance('address'),
+  townCity: factory.chance('city'),
+  postCode: factory.chance('postal'),
+  turnoverBand: factory.chance('pickone', ['<£100k', '£100k-£250k', '£500k-£750k']),
 });
 
 export default factory;
