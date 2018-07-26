e9d70efe57aa46bd77527a3726583399c1034521
Elias Malik <elias0789@gmail.com>
2018-07-26 14:29:14 +0100

    🗃 set timezone explicitly to UTC

--- api/database/migrations/sql/20180604_01_create_extensions.sql
+++ api/database/migrations/sql/20180604_01_create_extensions.sql
@@ -1,6 +1,8 @@
 /*
- * PostgreSQL extensions
+ * PostgreSQL extensions and settings
  */
 
 -- PostGIS for storing Community business lat/lng
 CREATE EXTENSION IF NOT EXISTS postgis;
+
+SET TIME ZONE 'UTC';
