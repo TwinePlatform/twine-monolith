88563490abc414eb314e356e2f4c7cf8f470a7a4
Elias Malik <elias0789@gmail.com>
2018-07-28 17:07:28 +0100

    🐛 remember to kill the knex client

--- api/tests/global_setup.js
+++ api/tests/global_setup.js
@@ -8,11 +8,13 @@ const knex = require('knex');
 const { getConfig } = require('../build/config');
 const { migrate } = require('../database');
 
-const config = getConfig(process.env.NODE_ENV);
-const client = knex(config.knex);
 
 module.exports = async () => {
+  const config = getConfig(process.env.NODE_ENV);
+  const client = knex(config.knex);
+
   await migrate.teardown();
   await client.migrate.latest();
   await client.seed.run();
+  await client.destroy();
 };
