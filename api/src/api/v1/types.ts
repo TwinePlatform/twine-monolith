3a69f5162ad753f8633717fec8a3f74bebe9c119
Elias Malik <elias0789@gmail.com>
2018-07-31 12:22:13 +0200

    🗜 add a general types file for the API plugin
    currently covers types for the api.json and the
    integration tests.

---
+++ api/src/api/v1/types.ts
@@ -0,0 +1,45 @@
+import * as Hapi from 'hapi';
+import { Dictionary } from 'ramda';
+import { ApiRequestQuery, ApiRequestBody } from './schema/request';
+import { ApiResponse } from './schema/response';
+
+/*
+ * api.json related types
+ */
+export enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export type ApiRouteSpec = {
+  description: string,
+  isImplemented: boolean,
+  auth: boolean,
+  intendedFor: any,
+  scope: string[],
+  query: ApiRequestQuery,
+  body: ApiRequestBody,
+  response: ApiResponse,
+};
+
+
+/*
+ * Test related types
+ */
+export type RouteTestFixture = {
+  name: string
+  setup?: (server: Hapi.Server) => Promise<void>
+  teardown?: (server: Hapi.Server) => Promise<void>
+  inject: {
+    url: string
+    method: HttpMethod
+    credentials?: object
+    payload?: object
+  }
+  expect: {
+    statusCode: number
+    payload?: object | ((a: Dictionary<any>) => void),
+  }
+};
