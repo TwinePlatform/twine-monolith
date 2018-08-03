f15415e99ed69c5ca6fe60ec3f0a0c577f26612b
Elias Malik <elias0789@gmail.com>
2018-08-03 18:19:09 +0200

    ♻️ consolidate extensions to hapi type declarations
    also initialise EmailService and attach to application state

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,20 +2,12 @@
  * Server initialisation and configuration
  */
 import * as Hapi from 'hapi';
-import * as Knex from 'knex';
 import v1 from './api/v1';
 import setup from './setup';
 import routes from './routes';
 import { Config } from '../config/types';
 import logger from './logger';
 
-// Extend declaration from hapi
-declare module 'hapi' {
-  interface ApplicationState {
-    config: Config;
-    knex: Knex;
-  }
-}
 
 const init = async (config: Config): Promise<Hapi.Server> => {
 
--- api/src/setup.ts
+++ api/src/setup.ts
@@ -5,19 +5,30 @@ import { Config } from '../config/types';
 
 import * as Hapi from 'hapi';
 import * as Knex from 'knex';
+import { emailInitialiser, EmailService } from './services/email';
 
+
+// Extend declaration from hapi
 declare module 'hapi' {
   interface Server {
     shutdown: (a?: boolean) => Promise<void>;
   }
+
   interface Request {
     knex: Knex;
+  }
+
+  interface ApplicationState {
+    config: Config;
+    knex: Knex;
+    EmailService: EmailService;
   }
 }
 
 export default (server: Hapi.Server, config: Config) => {
   server.app.config = config;
   server.app.knex = Knex(config.knex);
+  server.app.EmailService = emailInitialiser.init({ apiKey: config.email.postmark_key });
 
   server.decorate('request', 'knex', server.app.knex);
 
