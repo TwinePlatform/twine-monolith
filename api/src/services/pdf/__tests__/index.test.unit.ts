6524e604a66a498ca75b6c6897fcc7900e8a71ac
Elias Malik <elias0789@gmail.com>
2018-08-03 18:00:36 +0200

    👌 add failure case for PDF service test

--- api/src/services/pdf/__tests__/index.test.unit.ts
+++ api/src/services/pdf/__tests__/index.test.unit.ts
@@ -7,11 +7,22 @@ describe('PDF Service', () => {
   const { root } = getConfig(process.env.NODE_ENV);
 
   describe('fromTemplate', () => {
-    test('Visitor QR Code Template', async () => {
+    test('Render visitor QR code template', async () => {
       const qrCodeDataUrl = path.resolve(root, 'assets', 'png', 'power_to_change_logo_small.png');
       const pdfDocument = await fromTemplate(PdfTemplateEnum.VISITOR_QR_CODE, { qrCodeDataUrl });
       expect(pdfDocument.length).toBeGreaterThan(100);
       expect(typeof pdfDocument).toBe('string');
+    });
+
+    test('Fail to render the visitor QR code template', async () => {
+      expect.assertions(2);
+
+      try {
+        await fromTemplate(PdfTemplateEnum.VISITOR_QR_CODE, { foo: 'nonsense' });
+      } catch (error) {
+        expect(error).toBeTruthy();
+        expect(error).toEqual(expect.stringContaining('Unrecognized document structure'));
+      }
     });
   });
 });
