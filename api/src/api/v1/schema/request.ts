600b8d3cde6eb02e5da6de49e8f85df569a5e3b3
Elias Malik <elias0789@gmail.com>
2018-08-04 17:22:35 +0200

    ♻️ store common validation schema
    So they can be consistent as far as possible

--- api/src/api/v1/schema/request.ts
+++ api/src/api/v1/schema/request.ts
@@ -5,13 +5,17 @@
  * - Request query parameters
  * - Request path parameters
  * - Request payload
+ * - Commonly required payload parameters
  *
  * Used, overridden and extended by individual routes
  */
 import * as Joi from 'joi';
 import { Dictionary } from 'ramda';
+import { Map } from '../../../types/internal';
 
-
+/*
+ * Types
+ */
 export type ApiRequestQuery = {
   fields: string[]
   sort: string
@@ -22,10 +26,24 @@ export type ApiRequestQuery = {
 
 export type ApiRequestBody = Dictionary<any>;
 
-export const query = {
+
+/*
+ * Request query schema
+ */
+export const query: Map<keyof ApiRequestQuery, Joi.Schema> = {
   fields: Joi.array().items(Joi.string().min(1).max(255)),
   sort: Joi.string(),
   order: Joi.string(),
   offset: Joi.number().integer().min(0),
   limit: Joi.number().integer().min(1).max(100),
 };
+
+
+/*
+ * Commonly used payload schema
+ */
+export const gender =
+  Joi.string().only(['male', 'female', 'prefer not to say']);
+
+export const id =
+  Joi.number().integer().positive();
--- api/src/api/v1/users/schema.ts
+++ api/src/api/v1/users/schema.ts
@@ -1,5 +1,45 @@
 /*
  * Route validation schema for User routes
  */
-export { query } from '../schema/request';
+import * as Joi from 'joi';
+
+
+export { query, gender, id } from '../schema/request';
 export { response } from '../schema/response';
+
+const currentYear = (new Date()).getFullYear();
+
+/*
+ * Common request schema
+ */
+export const visitorName =
+  Joi.string()
+    .min(3)
+    .max(100)
+    .regex(/[^\w\s\d]/, { name: 'alphanumeric', invert: true });
+
+export const birthYear =
+  Joi.number()
+    .integer()
+    .min(1890)
+    .max(currentYear);
+
+export const email =
+  Joi.string()
+    .email();
+
+export const phoneNumber =
+  Joi.string()
+    .min(11)
+    .max(14);
+
+export const postCode =
+  Joi.string()
+    .min(4)
+    .max(10);
+
+export const emailConsent =
+  Joi.boolean();
+
+export const smsConsent =
+  Joi.boolean();
