6d4d9cda33a4ef6710b53c15c3eef923087db16d
Elias Malik <elias0789@gmail.com>
2018-08-06 17:50:14 +0200

    🚚 move `GET /organisations` into its own file

---
+++ api/src/api/v1/organisations/get.ts
@@ -0,0 +1,29 @@
+import * as Hapi from 'hapi';
+import { Organisations } from '../../../models';
+import { query, response } from './schema';
+
+
+export default [
+  {
+    method: 'GET',
+    path: '/organisations',
+    options: {
+      description: 'Retreive list of all organisations',
+      auth: {
+        strategy: 'standard',
+        access: {
+          scope: ['organisations_details-child:read'],
+        },
+      },
+      validate: { query },
+      response: { schema: response },
+    },
+    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+      const { knex } = request;
+
+      const org = await Organisations.get(knex);
+
+      return org.map(Organisations.serialise);
+    },
+  },
+];
--- api/src/api/v1/organisations/index.ts
+++ api/src/api/v1/organisations/index.ts
@@ -4,32 +4,8 @@
  * See also:
  * - /api/v1/api.json
  */
-import * as Hapi from 'hapi';
-import { Organisations } from '../../../models';
-import { query, response } from './schema';
-
+import Get from './get';
 
 export default [
-  {
-    method: 'GET',
-    path: '/organisations',
-    options: {
-      description: 'Retreive list of all organisations',
-      auth: {
-        strategy: 'standard',
-        access: {
-          scope: ['organisations_details-child:read'],
-        },
-      },
-      validate: { query },
-      response: { schema: response },
-    },
-    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-      const { knex } = request;
-
-      const org = await Organisations.get(knex);
-
-      return org.map(Organisations.serialise);
-    },
-  },
+  ...Get,
 ];
