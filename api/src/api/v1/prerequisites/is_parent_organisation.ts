eba33516115edf91bb6c0767390dd283550e2db3
Elias Malik <elias0789@gmail.com>
2018-08-17 20:37:27 +0200

    ✨ add pre-req for determining "parent" org relation
    Note that the funding body case is currently unimplemented

---
+++ api/src/api/v1/prerequisites/is_parent_organisation.ts
@@ -0,0 +1,65 @@
+/*
+ * "organisation_details-parent" route pre-requisite
+ *
+ * Determines if the authenticated user is trying to access an organisation
+ * which is actually a "parent" entity.
+ *
+ * Conditions under which this is true for organisation X:
+ * - User is a visitor for X
+ * - User is a volunteer for X
+ *
+ * Assumptions:
+ * - This route pre-requisite is run _AFTER_ the organisation in question
+ *   is fetched and placed in the `pre` object under one of the following keys:
+ *   > organisation
+ *   > communityBusiness
+ *   > fundingBody
+ */
+import * as Hapi from 'hapi';
+import { Organisation, CommunityBusiness } from '../../../models';
+import { OrganisationRequest } from '../types';
+import getOrganisation from './get_organisation';
+import { RoleEnum } from '../../../auth/types';
+import Roles from '../../../auth/roles';
+
+
+const getOrg = async (request: OrganisationRequest, h: Hapi.ResponseToolkit) => {
+  const { pre } = request;
+
+  if (pre.hasOwnProperty('organisation') || pre.hasOwnProperty('communityBusiness')) {
+    return <Organisation> pre.organisation || <CommunityBusiness> pre.communityBusiness;
+  }
+
+  // Fallback case: manually fetch organisation if not already cached.
+  return getOrganisation(request, h);
+};
+
+export default async (request: OrganisationRequest, h: Hapi.ResponseToolkit) => {
+  const { auth: { credentials }, server: { app: { knex } } } = request;
+  const { user } = credentials;
+
+  // TODO: Funding body case is unimplemented
+  //       See: https:github.com/TwinePlatform/twine-api/issues/XXX
+  const org = await getOrg(request, h);
+
+  const isVolunteer = await Roles.userHas(knex, {
+    role: RoleEnum.VOLUNTEER,
+    userId: user.id,
+    organisationId: org.id,
+  });
+
+  if (isVolunteer) {
+    return isVolunteer;
+  }
+
+  const isVisitor = await Roles.userHas(knex, {
+    role: RoleEnum.VISITOR,
+    userId: user.id,
+    organisationId: org.id,
+  });
+
+  // TODO: Funding body case is unimplemented
+  //       See: https:github.com/TwinePlatform/twine-api/issues/XXX
+
+  return isVisitor;
+};
