cedc7d287813ba4df13abbf51cd8464c3c13f2a3
AstroAsh <astroaisha@gmail.com>
2018-09-19 11:01:08 +0100

    👌🏽 add resticted access permission rather than replace
     #157"

--- api/database/migrations/sql/20180918_18_add_user_details_permission_restricted.sql
+++ api/database/migrations/sql/20180918_18_add_user_details_permission_restricted.sql
@@ -3,19 +3,21 @@
  * for creating new visitors on the visitor app
  */
 
- UPDATE access_role_permission
-SET
-  access_mode = 'restricted'
-WHERE access_role_id = (
+INSERT INTO access_role_permission (access_role_id, permission_id, access_mode)
+
+VALUES (
+  (
     SELECT access_role_id
     FROM access_role
     WHERE access_role_name = 'ORG_ADMIN'
-  ) 
-  AND permission_id = (
+  ),
+  (
     SELECT permission_id
     FROM permission
     WHERE permission_entity = 'user_details'
       AND permission_level = 'child'
       AND access_type = 'write'
-  );
+  ),
+  'restricted'
+);
   
