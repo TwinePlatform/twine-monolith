442ec7134ee5c863da389a7f5b4dcfe80d0c2281
Elias Malik <elias0789@gmail.com>
2018-09-25 12:40:33 +0100

    🗜 use conditional typings to not break arrays
    Use conditional types to only apply deep partial
    if the element is not an array. If it is an
    array, apply it to its elements instead.

--- api/src/types/internal.d.ts
+++ api/src/types/internal.d.ts
@@ -11,8 +11,16 @@ export type Dictionary<T> = {
 
 export type ValueOf<T> = T[keyof T]
 
+/*
+ * Recursive definition of deep partial without breaking array types
+ */
 export type DeepPartial<T> = {
-  [P in keyof T]?: DeepPartial<T[P]>
+  [P in keyof T]?:
+    T[P] extends Array<infer U>
+      ? Array<DeepPartial<U>>
+      : T[P] extends ReadonlyArray<infer U>
+        ? ReadonlyArray<DeepPartial<U>>
+        : DeepPartial<T[P]>
 }
 
 export type JsonPrimitives =
