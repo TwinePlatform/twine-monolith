63b621d07646467bcf0e33fc3ad42b03fd98f553
AstroAsh <astroaisha@gmail.com>
2018-10-02 12:50:08 +0100

    ♻️ fold cb/:id/volunteers & cb/me/volunteers into 1 route
    - comment isChild check to explain how it checks for user type if /:id/ is passed
    - write tests for different user types

--- api/src/api/v1/community_businesses/volunteers/__tests__/get.test.integration.ts
+++ api/src/api/v1/community_businesses/volunteers/__tests__/get.test.integration.ts
@@ -2,23 +2,24 @@ import * as Hapi from 'hapi';
 import * as Knex from 'knex';
 import { init } from '../../../../../server';
 import { getConfig } from '../../../../../../config';
-import { User, Users, Organisation, Organisations } from '../../../../../models';
+import { User, CommunityBusinesses, Organisation, Volunteers } from '../../../../../models';
 import { RoleEnum } from '../../../../../auth/types';
 
 
 describe('API /community-businesses/{id}/volunteers', () => {
   let server: Hapi.Server;
   let knex: Knex;
-  let user: User;
   let organisation: Organisation;
+  let volunteerAdmin: User;
+  let orgAdmin: User;
   const config = getConfig(process.env.NODE_ENV);
 
   beforeAll(async () => {
     server = await init(config);
     knex = server.app.knex;
-
-    user = await Users.getOne(knex, { where: { name: 'Gordon' } });
-    organisation = await Organisations.getOne(knex, { where: { id: 2 } });
+    organisation = await CommunityBusinesses.getOne(knex, { where: { id: 2 } });
+    volunteerAdmin = await Volunteers.getOne(knex, { where: { id: 7 } });
+    orgAdmin = await Volunteers.getOne(knex, { where: { id: 5 } });
   });
 
   afterAll(async () => {
@@ -26,13 +27,47 @@ describe('API /community-businesses/{id}/volunteers', () => {
   });
 
   describe('GET /community-businesses/{id}/volunteers', () => {
-    test('happy path', async () => {
+    test(':: success - Volunteer Admin can access volunteer details for their org', async () => {
       const res = await server.inject({
         method: 'GET',
         url: '/v1/community-businesses/me/volunteers',
         credentials: {
           organisation,
-          user,
+          volunteerAdmin,
+          scope: ['user_details-sibling:read'],
+        },
+      });
+      expect(res.statusCode).toBe(200);
+      expect((<any> res.result).result).toHaveLength(2);
+      expect((<any> res.result).result[0]).toEqual(expect.objectContaining({
+        name: 'Emma Emmerich',
+        deletedAt: null,
+      }));
+      expect((<any> res.result).meta).toEqual({ total: 2 });
+    });
+
+    test(':: success with offset', async () => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/community-businesses/me/volunteers?offset=1',
+        credentials: {
+          organisation,
+          user: volunteerAdmin,
+          scope: ['user_details-sibling:read'],
+        },
+      });
+      expect(res.statusCode).toBe(200);
+      expect((<any> res.result).result).toHaveLength(1);
+      expect((<any> res.result).meta).toEqual({ total: 2 });
+    });
+
+    test(':: success - Org Admin can access volunteer details for their org', async () => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/community-businesses/me/volunteers',
+        credentials: {
+          organisation,
+          user: orgAdmin,
           scope: ['user_details-child:read'],
         },
       });
@@ -45,22 +80,33 @@ describe('API /community-businesses/{id}/volunteers', () => {
       expect((<any> res.result).meta).toEqual({ total: 2 });
     });
 
-    test('happy path with offset', async () => {
+    test(':: fail - Volunteer Admin cannot access volunteer details for another org', async () => {
       const res = await server.inject({
         method: 'GET',
-        url: '/v1/community-businesses/me/volunteers?offset=1',
+        url: '/v1/community-businesses/1/volunteers',
         credentials: {
           organisation,
-          user,
+          user: volunteerAdmin,
           scope: ['user_details-child:read'],
         },
       });
-      expect(res.statusCode).toBe(200);
-      expect((<any> res.result).result).toHaveLength(1);
-      expect((<any> res.result).meta).toEqual({ total: 2 });
+      expect(res.statusCode).toBe(403);
     });
 
-    test('query child organisation as TWINE_ADMIN', async () => {
+    test(':: fail - Org Admin cannot access volunteer details for another org', async () => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/community-businesses/1/volunteers',
+        credentials: {
+          organisation,
+          user: orgAdmin,
+          scope: ['user_details-child:read'],
+        },
+      });
+      expect(res.statusCode).toBe(403);
+    });
+
+    test(':: success - TWINE_ADMIN can access child organisation', async () => {
       const res = await server.inject({
         method: 'GET',
         url: '/v1/community-businesses/2/volunteers',
--- api/src/api/v1/community_businesses/volunteers/get.ts
+++ api/src/api/v1/community_businesses/volunteers/get.ts
@@ -2,8 +2,8 @@ import * as Hapi from 'hapi';
 import * as Boom from 'boom';
 import { has, pick, omit } from 'ramda';
 import { User, ModelQuery, Volunteers } from '../../../../models';
-import { query, filterQuery, response } from '../../users/schema';
-import { id } from '../schema';
+import { query, response } from '../../users/schema';
+import { meOrId } from '../schema';
 import { getCommunityBusiness, isChildOrganisation } from '../../prerequisites';
 import { GetAllVolunteersRequest } from '../../types';
 
@@ -17,11 +17,11 @@ const routes: Hapi.ServerRoute[] = [
       auth: {
         strategy: 'standard',
         access: {
-          scope: ['user_details-child:read'],
+          scope: ['user_details-child:read', 'user_details-sibling:read'],
         },
       },
       validate: {
-        params: { organisationId: id },
+        params: { organisationId: meOrId },
         query: {
           ...query,
         },
@@ -36,67 +36,13 @@ const routes: Hapi.ServerRoute[] = [
     handler: async (request: GetAllVolunteersRequest, h: Hapi.ResponseToolkit) => {
       const { query, pre: { communityBusiness, isChild }, server: { app: { knex } } } = request;
 
+      /*
+       * if id !== 'me' checks if user is a parent to accessed org
+       * this only grants access to TWINE_ADMIN & FUNDING_BODY users
+       */
       if (request.params.organisationId !== 'me' && !isChild) {
         return Boom.forbidden('Insufficient permissions to access this resource');
       }
-
-      const q: {
-        limit?: number,
-        offset?: number,
-        order?: [string, 'asc' | 'desc']
-      } = {
-        ...pick(['limit', 'offset'], query),
-        order: query.sort ? [query.sort, query.order || 'asc'] : undefined,
-      };
-
-      const modelQuery: ModelQuery<User> = { ...q, where: { deletedAt: null } };
-
-      // TODO: Need to actually filtering and field option
-
-      const volunteers =
-        await Volunteers.fromCommunityBusiness(knex, communityBusiness, modelQuery);
-
-      const count = has('limit', modelQuery) || has('offset', modelQuery)
-        ? await Volunteers.fromCommunityBusiness(
-            knex,
-            communityBusiness,
-            omit(['limit', 'offset'], modelQuery)
-          )
-          .then((res) => res.length)
-        : volunteers.length;
-
-      return {
-        result: await Promise.all(volunteers.map(Volunteers.serialise)),
-        meta: { total: count },
-      };
-    },
-  },
-
-  {
-    method: 'GET',
-    path: '/community-businesses/me/volunteers',
-    options: {
-      description: 'Retreive list of all volunteers from an organisation',
-      auth: {
-        strategy: 'standard',
-        access: {
-          scope: ['user_details-child:read', 'user_details-sibling:read'],
-        },
-      },
-      validate: {
-        query: {
-          ...query,
-          ...filterQuery,
-        },
-        failAction: (request, h, err) => err,
-      },
-      response: { schema: response },
-      pre: [
-        { method: getCommunityBusiness, assign: 'communityBusiness' },
-      ],
-    },
-    handler: async (request: GetAllVolunteersRequest, h: Hapi.ResponseToolkit) => {
-      const { query, pre: { communityBusiness }, server: { app: { knex } } } = request;
 
       const q: {
         limit?: number,
