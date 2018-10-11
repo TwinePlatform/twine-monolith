33199cd66968eced519cd8400dcedc34d1ce74e5
AstroAsh <astroaisha@gmail.com>
2018-10-11 18:42:49 +0100

    👌🏽 regions/cbs -> regions/:id/cbs
    - use Cbs.get method
    - anonymise data in hander
    - update tests to cover more edge cases

--- api/src/api/v1/regions/__tests__/community_businesses.test.integration.ts
+++ api/src/api/v1/regions/__tests__/community_businesses.test.integration.ts
@@ -2,32 +2,86 @@
  * User API functional tests
  */
 import * as Hapi from 'hapi';
+import * as Knex from 'knex';
 import { init } from '../../../../server';
 import { getConfig } from '../../../../../config';
+import { getTrx } from '../../../../../tests/utils/database';
+import { CommunityBusinesses } from '../../../../models';
+import { RegionEnum, SectorEnum } from '../../../../models/types';
 
 describe('API /users', () => {
   let server: Hapi.Server;
   const config = getConfig(process.env.NODE_ENV);
+  const knex = Knex(config.knex);
+
+  let trx: Knex.Transaction;
 
   beforeAll(async () => {
     server = await init(config);
   });
 
+  beforeEach(async () => {
+    trx = await getTrx(knex);
+  });
+
+  afterEach(async () => {
+    await trx.rollback();
+  });
+
   afterAll(async () => {
+    await knex.destroy();
     await server.shutdown(true);
   });
 
-  describe('GET /regions/community-businesses', () => {
-    test('happy path', async () => {
+  describe('GET /regions/:id/community-businesses', () => {
+    test(':: success - returns list of all cbs in region', async () => {
       const res = await server.inject({
         method: 'GET',
-        url: '/v1/regions/community-businesses',
+        url: '/v1/regions/3/community-businesses',
       });
 
       expect(res.statusCode).toBe(200);
-      expect((<any> res.result).result).toEqual({
-        London: [{ id: 1, name: 'Aperture Science' }, { id: 2, name: 'Black Mesa Research' }],
+      expect((<any> res.result).result).toEqual(
+        [{ id: 1, name: 'Aperture Science' }, { id: 2, name: 'Black Mesa Research' }]);
+    });
+
+    test(':: success - returns empty list of unpopulated region', async () => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/regions/1/community-businesses',
       });
+
+      expect(res.statusCode).toBe(200);
+      expect((<any> res.result).result).toEqual([]);
+    });
+
+    test(':: fail - returns 404 for nonexisting region', async () => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/regions/100/community-businesses',
+      });
+
+      expect(res.statusCode).toBe(404);
+    });
+
+    test(':: success - returns list in alphabetical order', async () => {
+      await CommunityBusinesses.add(trx, {
+        name: 'Zeldas Dungeon',
+        region: RegionEnum.LONDON,
+        sector: SectorEnum.COMMUNITY_HUB,
+      });
+      await CommunityBusinesses.add(trx, {
+        name: 'Okamis Fields',
+        region: RegionEnum.LONDON,
+        sector: SectorEnum.ENVIRONMENT,
+      });
+
+      const res = await server.inject({
+        method: 'GET',
+        url: '/v1/regions/3/community-businesses',
+      });
+
+      expect(res.statusCode).toBe(404);
     });
   });
 });
--- api/src/api/v1/regions/community_businesses.ts
+++ api/src/api/v1/regions/community_businesses.ts
@@ -1,4 +1,6 @@
 import * as Hapi from 'hapi';
+import * as Boom from 'boom';
+import { pick, map } from 'ramda';
 import { CommunityBusinesses } from '../../../models';
 import { response } from '../schema/response';
 
@@ -6,16 +8,31 @@ import { response } from '../schema/response';
 export default [
   {
     method: 'GET',
-    path: '/regions/community-businesses',
+    path: '/regions/{regionId}/community-businesses',
     options: {
       description: 'Retreive list of regions with related community businesses',
       auth: false,
       response: { schema: response },
     },
     handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-      const { server: { app: { knex } } } = request;
+      const { server: { app: { knex } }, params: { regionId } } = request;
 
-      return CommunityBusinesses.byRegion(knex, { where: { deletedAt: null } });
+      const [{ regionName } = { regionName: null }] = await knex('community_business_region')
+        .select({ regionName: 'region_name' })
+        .where({ community_business_region_id: regionId })
+        .limit(1);
+
+      if (!regionName) { return Boom.notFound('Region cannot be found'); }
+
+      const regions = await CommunityBusinesses.get(knex, {
+        where: {
+          region: regionName,
+          deletedAt: null,
+        },
+        order: ['organisation_name', 'asc'],
+      });
+
+      return map(pick(['id', 'name']), regions);
     },
   },
 ] as Hapi.ServerRoute[];
