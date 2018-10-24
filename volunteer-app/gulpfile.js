2c49bdffee8e198c6e1cc21da90a7901e973ee19
Elias Malik <elias0789@gmail.com>
2018-10-24 10:41:18 +0100

    🔥 nerf the SASS gulp task since it breaks the CSS

--- volunteer-app/gulpfile.js
+++ volunteer-app/gulpfile.js
@@ -10,19 +10,20 @@ var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', []);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+  return done();
+  // gulp.src('./scss/ionic.app.scss')
+  //   .pipe(sass())
+  //   .on('error', sass.logError)
+  //   .pipe(gulp.dest('./www/css/'))
+  //   .pipe(minifyCss({
+  //     keepSpecialComments: 0
+  //   }))
+  //   .pipe(rename({ extname: '.min.css' }))
+  //   .pipe(gulp.dest('./www/css/'))
+  //   .on('end', done);
 });
 
 gulp.task('watch', function() {
