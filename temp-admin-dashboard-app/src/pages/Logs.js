ec4d8a5fba4d401e5cf701353c4db0703369f81b
AstroAsh <astroaisha@gmail.com>
2018-10-25 17:03:27 +0100

    ✨ Logs page downloads csv

--- temp-admin-dashboard-app/src/pages/Logs.js
+++ temp-admin-dashboard-app/src/pages/Logs.js
@@ -1,10 +1,107 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import * as csv from 'fast-csv'
+import saveAs from 'file-saver';
+import * as moment from 'moment'
 
-export default () => {
-  return (
-    <Typography variant="h5" component="h3">
-      Logs
-    </Typography>
-  )
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+import { api } from '../utils/api';
+
+const today = moment().format('YYYY-MM-DD');
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  snackbar: {
+    margin: theme.spacing.unit,
+  },
+});
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+  };
+
+  getVisitLogs = () => {
+    api.visitLogs()
+      .then((res) => {
+        const logs = res.data.result
+        const headers = Object.keys(logs[0]).reduce((acc, el) => ({ ...acc, ...{ [el]: el } }), { date: 'date', time: 'time' })
+
+        const csvData = [headers].concat(logs.map(x => (
+          {
+            ...x,
+            ...{
+              date: moment(x.createdAt).format('DD-MM-YY'),
+              time: moment(x.createdAt).format('HH:MM')
+            }
+          })))
+        csv.writeToString(csvData, (err, csvString) => {
+          var file = new File([csvString], `${today}_visit_logs.csv`, { type: "text/csv" });
+          saveAs(file)
+        })
+      })
+      .catch(console.log)
+  }
+  getVolunteerLogs = () => {
+    api.volunteerLogs()
+      .then((res) => {
+        const logs = res.data.result
+        const headers = Object.keys(logs[0]).reduce((acc, el) => ({ ...acc, ...{ [el]: el } }), { date: 'date', time: 'time' })
+
+        const csvData = [headers].concat(logs.map(x => (
+          {
+            ...x,
+            ...{
+              date: moment(x.createdAt).format('DD-MM-YY'),
+              time: moment(x.createdAt).format('HH:MM')
+            }
+          })))
+
+        csv.writeToString(csvData, (err, csvString) => {
+          var file = new File([csvString], `${today}_volunteer_logs.csv`, { type: "text/csv" });
+          saveAs(file)
+        })
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    const { props: { classes }, getVisitLogs, getVolunteerLogs } = this;
+    return (
+      <Fragment>
+        <Typography variant="h5" component="h3">
+          Home
+        </Typography>
+        <Button component="span" color="secondary" onClick={() => getVisitLogs()}>
+          Last Months Visitor Logs
+        </Button>
+        <Button component="span" color="secondary" onClick={() => getVolunteerLogs()}>
+          Last Months Volunteer Logs
+        </Button>
+      </Fragment>
+    )
+  }
 };
+
+export default withStyles(styles)(Home)
--- temp-admin-dashboard-app/src/utils/api.js
+++ temp-admin-dashboard-app/src/utils/api.js
@@ -1,7 +1,6 @@
 import _axios from 'axios';
 import { RoleEnum } from './enums'
-import AdminCodes from '../pages/AdminCodes';
-
+import * as moment from 'moment'
 
 const baseURL = process.env
   ? process.env.NODE_ENV === 'development'
@@ -15,6 +14,10 @@ const axios = _axios.create({
   withCredentials: true,
 });
 
+const today = moment();
+const tomorrow = today.clone().add(1, 'day').format('YYYY-MM-DD');
+const lastMonth = today.clone().subtract(1, 'month').format('YYYY-MM-DD');
+
 export const api = {
   login: ({ email, password }) => {
     return axios.post('/users/login', { email, password, restrict: RoleEnum.TwineAdmin })
@@ -27,5 +30,11 @@ export const api = {
   },
   organisation: (id) => {
     return axios.get(`/community-businesses/${id}`)
+  },
+  visitLogs: () => {
+    return axios.get(`/visit-logs?since=${lastMonth}&until=${tomorrow}`)
+  },
+  volunteerLogs: () => {
+    return axios.get(`/volunteer-logs?since=${lastMonth}&until=${tomorrow}`)
   },
 };
