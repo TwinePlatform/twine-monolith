938b65223ed23e052a15a75e174769e5d5a08328
Elias Malik <elias0789@gmail.com>
2018-11-02 11:27:23 +0000

    🔊 log response/request payloads and headers

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -3,34 +3,39 @@ import { Environment } from '../config/types';
 const good = require('good');
 
 
-const getOption = (env: Environment) => {
-  /* istanbul ignore next */
-  const reporters = env === Environment.TESTING
-    ? {}
-    : {
-      myConsoleReporter: [{
-        module: 'good-squeeze',
-        name: 'Squeeze',
-        args: [{ log: '*', response: '*' }],
-      }, {
-        module: 'good-console',
-      }, 'stdout'],
-    };
-  return {
-    ops: {
-      interval: 1000,
+const options = {
+  includes: {
+    request: ['headers', 'payload'],
+    response: ['headers', 'payload'],
+    reporters: {
+      console: [
+        {
+          module: 'good-squeeze',
+          name: 'Squeeze',
+          args: [{ log: '*', response: '*' }],
+        },
+        {
+          module: 'good-console',
+        },
+        'stdout',
+      ],
     },
-    reporters,
-  };
+  },
 };
+
+
+const getOption = (env: Environment) =>
+  /* istanbul ignore next */
+  env === Environment.TESTING
+    ? {}
+    : options;
+
 
 export default {
   name: 'twine-logger',
-  register: async (server: Hapi.Server, options: {env: Environment}) => {
-    await server.register({
+  register: async (server: Hapi.Server, options: {env: Environment}) =>
+    server.register({
       plugin: good,
       options: getOption(options.env),
-    });
-    return;
-  },
+    }),
 };
