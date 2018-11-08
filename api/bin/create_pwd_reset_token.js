a3784e40988319c734e853b181c2c4b434ede29c
Elias Malik <elias0789@gmail.com>
2018-11-08 10:22:23 +0000

    🚚 move scripts to bin folder

--- api/bin/create_pwd_reset_token.js
+++ api/bin/create_pwd_reset_token.js
@@ -1,17 +1,20 @@
-// script for creating a new user locally
+#!/usr/bin/env node
+
+/*
+ * script for creating a new password reset token locally
+ */
+
 const Knex = require('knex')
 const {Users} = require('../build/src/models/user');
 const {getConfig} = require('../build/config')
 
-const go = async() => {
+(async () => {
   const {knex: config} = getConfig('development')
-  
+
   const client = Knex(config)
-  
+
   const user = await Users.getOne(client, { where: { id: 1 }})
   await Users.createPasswordResetToken(client, user)
 
   return client.destroy();
-}
-
-go();
+})();
--- api/bin/create_user.js
+++ api/bin/create_user.js
@@ -1,14 +1,19 @@
-// script for creating a new user locally
+#!/usr/bin/env node
+
+/*
+ * script for creating a new user locally
+ */
+
 const Knex = require('knex')
 const {Volunteers} = require('../build/src/models/volunteer');
 const {CommunityBusinesses} = require('../build/src/models/community_business.js');
 const {getConfig} = require('../build/config')
 
-const go = async() => {
+(async () => {
   const {knex: config} = getConfig('development')
-  
+
   const client = Knex(config)
-  
+
   const user = {
     email: 'INSERTEMAIL@EMAIL.COM',
     password: 'Password123!',
@@ -16,10 +21,9 @@ const go = async() => {
   }
   const cb = await CommunityBusinesses.getOne(client, {where: {id:1}})
   console.log({cb});
-  
+
   const volunteer = await Volunteers.addWithRole(client, user, 'VOLUNTEER_ADMIN', cb, '10101')
   console.log({volunteer});
-  return client.destroy();
-}
 
-go()
+  await client.destroy();
+})();
