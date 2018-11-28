092395c7d73b04b19b224b54247ed3d9de62a8e9
Elias Malik <elias0789@gmail.com>
2018-11-28 12:59:32 +0000

    🗜 update auth typings to better reflect DB
    Alphabetise resource enums, add new entries, add "sibling"

--- api/src/auth/types.ts
+++ api/src/auth/types.ts
@@ -26,22 +26,23 @@ export enum AccessEnum {
 export enum ResourceEnum {
   CONSTANTS = 'constants',
   ORG_DETAILS = 'organisations_details',
-  ORG_SUBSCRIPTIONS = 'organisations_subscriptions',
   ORG_FEEDBACK = 'organisations_feedback',
-  ORG_TRAINING = 'organisations_training',
   ORG_INVITATIONS = 'organisations_invitations',
   ORG_OUTREACH = 'organisations_outreach',
+  ORG_SUBSCRIPTIONS = 'organisations_subscriptions',
+  ORG_TRAINING = 'organisations_training',
+  ORG_VOLUNTEERS = 'organisations_volunteers',
   USER_DETAILS = 'user_details',
-  VOLUNTEER_ACTIVITIES = 'volunteer_activities',
-  VOLUNTEER_LOGS = 'volunteer_logs',
   VISIT_ACTIVITIES = 'visit_activities',
   VISIT_LOGS = 'visit_logs',
+  VOLUNTEER_LOGS = 'volunteer_logs',
 }
 
 export enum PermissionLevelEnum {
   OWN = 'own',
   CHILD = 'child',
   PARENT = 'parent',
+  SIBLING = 'sibling',
   ALL = 'all',
 }
 
