b9577aeb8fbcd07ca0685f8ce5df5a598d6f4dd8
AstroAsh <astroaisha@gmail.com>
2019-01-25 15:05:56 +0100

    💫 register/visitors update (#318)
    💫 register/visitors to allow anon user creation & number OR email to identify
    
    - update route to take isAnonymous boolean
    - refactor route to use Visitor.add(Anonymous)WithRole methods
    - allow either phone number or email as identifying data
    - add tests for new use cases
    
    fixes #298 
    TwinePlatform/twine-visitor#536
    
    **Please squash and merge**
    
    User Flows:
    - _see partnered PR_

--- api/src/api/v1/users/register/visitors.ts
+++ api/src/api/v1/users/register/visitors.ts
@@ -3,6 +3,7 @@
  */
 import * as Hapi from 'hapi';
 import * as Boom from 'boom';
+import * as Joi from 'joi';
 import {
   id,
   userName,
@@ -24,8 +25,6 @@ import {
 import * as QRCode from '../../../../services/qrcode';
 import * as PdfService from '../../../../services/pdf';
 import { EmailTemplate } from '../../../../services/email/templates';
-import { RoleEnum } from '../../../../auth/types';
-import Roles from '../../../../auth/roles';
 import { RegisterRequest } from '../../types';
 import { StandardCredentials } from '../../../../auth/strategies/standard';
 
@@ -44,11 +43,12 @@ export default [
           name: userName.required(),
           gender: gender.required(),
           birthYear: birthYear.default(null),
-          email: email.required(),
+          email: email.allow(''),
           phoneNumber: phoneNumber.allow(''),
           postCode: postCode.allow(''),
           emailConsent: isEmailConsentGranted.default(false),
           smsConsent: isSMSConsentGranted.default(false),
+          isAnonymous: Joi.boolean().default(false) ,
         },
       },
       response: { schema: response },
@@ -64,12 +64,28 @@ export default [
        * Preliminaries
        * (Can possibly eventually be removed into pre-requisites)
        */
-      // Check user doesn't already exist
-      if (await Users.exists(knex, { where: { email: payload.email } })) {
+
+      // Checks for standard Visitors
+      if (!payload.isAnonymous) {
+        // Check email or phone number is supplied
+        if (!(payload.email || payload.phoneNumber)) {
+          return Boom.badRequest('Please supply either email or phone number');
+        }
+        // Check user doesn't already exist
+        if (payload.email && await Users.exists(knex, { where: { email: payload.email } })) {
         // Registering second roles is not yet supported;
         // see https://github.com/TwinePlatform/twine-api/issues/247#issuecomment-443182884
-        throw Boom.conflict('User with this e-mail already registered');
+          throw Boom.conflict('User with this e-mail already registered');
+        }
+        if (payload.phoneNumber
+          && await Users.exists(knex, { where: { phoneNumber: payload.phoneNumber } })
+          ) {
+        // Registering second roles is not yet supported;
+        // see https://github.com/TwinePlatform/twine-api/issues/247#issuecomment-443182884
+          throw Boom.conflict('User with this phone number already registered');
+        }
       }
+
       if (organisation && organisation.id !== payload.organisationId) {
         throw Boom.badRequest('Cannot register visitor for different organisation');
       }
@@ -77,7 +93,7 @@ export default [
         throw Boom.badRequest('Unrecognised organisation');
       }
 
-      // Fetch the community business admin
+      // Fetch the community business
       const cb = await CommunityBusinesses.getOne(knex, {
         where: { id: payload.organisationId, deletedAt: null },
       });
@@ -94,32 +110,28 @@ export default [
       /*
        * Main actions
        */
+
       // Create the visitor
-      // // TODO: This should actually be part of the Visitor model
-      // //       However, this breaks the typings and it's not obvious
-      // //       right now how to accomodate it.
-      const visitor = await knex.transaction(async (trx) => {
-        const visitor = await Visitors.add(trx, Visitors.create(payload));
-        await Roles.add(trx, {
-          role: RoleEnum.VISITOR,
-          userId: visitor.id,
-          organisationId: payload.organisationId,
-        });
-        return visitor;
-      });
+      const visitor = payload.isAnonymous
+          ? await Visitors.addAnonymousWithRole(knex, cb, Visitors.create(payload))
+          : await Visitors.addWithRole(knex, cb, Visitors.create(payload));
 
       // Send QR code to visitor
-      // // generate QR code data URL
+      // generate QR code data URL
       const qrCode = await QRCode.create(visitor.qrCode);
-      // // create PDF blob
+      // create PDF blob
       const document = await PdfService.fromTemplate(
         PdfService.PdfTemplateEnum.VISITOR_QR_CODE,
         { qrCodeDataUrl: qrCode }
       );
 
-      // // send email to visitor QR code attached
-      // // send sign-up notification to cb-admin
-      // // // TODO: This should be hidden away
+      /*
+      * Auxilliary actions
+      */
+
+      // send email to visitor QR code attached
+      // send sign-up notification to cb-admin
+      // TODO: This should be hidden away
       await EmailService.sendBatch([
         {
           from: 'visitorapp@powertochange.org.uk', // this shouldn't be hardcoded
@@ -145,9 +157,6 @@ export default [
         },
       ]);
 
-      /*
-       * Auxilliary actions
-       */
       // Register login event
       await Visitors.recordLogin(knex, visitor);
 
