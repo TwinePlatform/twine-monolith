6c11149c537c0ae1cdbd5fc36907a3fa1b49bb5e
AstroAsh <astroaisha@gmail.com>
2019-04-19 19:29:28 +0200

    🚨 remove usused imports

--- dashboard-app/src/dashboard/__tests__/Dashboard.test.ts
+++ dashboard-app/src/dashboard/__tests__/Dashboard.test.ts
@@ -1,8 +1,6 @@
 import MockAdapter from 'axios-mock-adapter';
 import {
-  fireEvent,
   cleanup,
-  waitForElement,
   wait,
 } from 'react-testing-library';
 import { renderWithHistory } from '../../util/tests';
