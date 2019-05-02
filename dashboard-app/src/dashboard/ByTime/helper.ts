4a0d7d4e979dd530f1fb4874644fe29adeb0adf4
AstroAsh <astroaisha@gmail.com>
2019-05-02 20:26:16 +0200

    ⏱ add real times with Duration utils

--- dashboard-app/src/dashboard/ByTime/helper.ts
+++ dashboard-app/src/dashboard/ByTime/helper.ts
@@ -1,28 +1,34 @@
-import { DataTableRow } from '../../components/DataTable/types';
-import DateRange from '../../util/dateRange';
-import { UnitEnum } from '../../types';
+import { Duration } from 'twine-util';
 import { assocPath, mergeAll } from 'ramda';
+import moment from 'moment';
+import { DataTableRow } from '../../components/DataTable/types';
+import { UnitEnum } from '../../types';
+import { roundToDecimal } from '../../util/mathUtil';
 
 interface Params { data: any[]; months: string[]; unit: UnitEnum; }
 
 export const timeLogsToTable = ({ data, months, unit }: Params) =>
   data.reduce((acc: DataTableRow[], el: any) => {
     const activityExists = acc.some((x) => x.columns.Activity.content === el.activity);
+    const logMonth = moment(el.createdAt).format('MMMM');
     if (activityExists) {
       return acc.map((x) => {
         if (x.columns.Activity.content === el.activity) {
-          const logMonth = DateRange.months[new Date(el.createdAt).getMonth()];
           x.columns[logMonth].content = Number(x.columns[logMonth].content) + 1;
         }
         return x;
       });
     }
-    const monthsContent = months.map((m: string) => ({ [m]: { content: 1 } }));
+    const monthsContent = months.map((m: string) => ({ [m]: { content: 0 } }));
     const newRow = {
       columns: {
         Activity: { content: el.activity, },
         [`Total ${unit}`]: { content: 0 },
         ...mergeAll(monthsContent),
+        [logMonth]: { content: unit === UnitEnum.HOURS
+          ? roundToDecimal(Duration.toHours(el.duration))
+          : roundToDecimal(Duration.toWorkingDays(el.duration)),
+        },
       },
     };
     return acc.concat(newRow);
---
+++ dashboard-app/src/util/mathUtil.ts
@@ -0,0 +1,1 @@
+export const roundToDecimal = (n: number) => (Math.round(n * 100) / 100);
