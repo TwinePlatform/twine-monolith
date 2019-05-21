7f7763d161d846fdb234816c13b6e4db109ec203
Elias Malik <elias0789@gmail.com>
2019-05-21 10:59:29 +0100

    sort out totals calculation

--- dashboard-app/src/components/DataTable/util.ts
+++ dashboard-app/src/components/DataTable/util.ts
@@ -1,27 +1,23 @@
-import { sum, map, filter, compose, complement } from 'ramda';
+import { sum, transpose } from 'ramda';
+import { MathUtil } from 'twine-util';
 import { TotalsRowProps, DataTableRow, RowProps } from './types';
 
 
-export const extract = (rows: TotalsRowProps['rows']) =>
-rows.map((row) => row.order.map((h) => row.columns[h].content));
+export const extractContent = (rows: TotalsRowProps['rows']) =>
+  rows.map((row) => row.order.map((h) => row.columns[h].content));
 
-export const transpose = <T>(A: T[][]) =>
-A[0].map((col, i) => A.map((row) => row[i]));
-
-const x = map(filter(complement(isNaN)));
-const y = filter(complement(isNaN));
-const z = complement(isNaN);
-
-export const make = compose(
-  map(sum),
-  map(filter(complement(isNaN))),
-  map(map(Number)),
-  transpose,
-  extract
-);
+// Had to do this with native maps because map/compose type inference
+// ended up being screwy and inferring objects instead of arrays
+export const calculateTotals = (rs: TotalsRowProps['rows']) =>
+  transpose(extractContent(rs))
+    .map((xs) => xs.map(Number))
+    .map((xs) => sum(xs))
+;
 
 export const toRowProps = (r: DataTableRow, headers: string[]): RowProps =>
   ({
     columns: r.columns,
     order: headers,
   });
+
+export const formatNumber = MathUtil.roundTo(2);
