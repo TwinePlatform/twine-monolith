d8bf20de7116d2c7846a409ac3e16a1ccc67439f
AstroAsh <astroaisha@gmail.com>
2019-05-24 14:35:08 +0100

    🗃 replace special character ellipis in volunteer activity (#116)
    fixes #99

---
+++ api/database/migrations/sql/20190524_replace-ellipsis.sql
@@ -0,0 +1,7 @@
+/*
+ * Migration template
+ */
+
+ UPDATE volunteer_activity
+  SET volunteer_activity_name = 'Helping with raising funds (shop, events...)'
+  WHERE volunteer_activity_name = 'Helping with raising funds (shop, events…)';
