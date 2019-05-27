5f6fcdd368202f625d7250db96908ac9db82ac6c
Elias Malik <elias0789@gmail.com>
2019-05-27 14:22:34 +0100

    simplify

--- dashboard-app/src/util/dataManipulation/aggregatedToCsv.ts
+++ dashboard-app/src/util/dataManipulation/aggregatedToCsv.ts
@@ -1,17 +1,13 @@
 import csv from 'fast-csv';
 import { AggregatedData } from './logsToAggregatedData';
-import { aggregatedToTableData } from './aggregatedToTableData';
+import { calculateTotalsUsing } from './aggregatedToTableData';
 import { DurationUnitEnum } from '../../types';
-import { Objects } from 'twine-util';
 
 
 // tslint:disable-next-line: max-line-length
 export const aggregatedToCsv = async (data: AggregatedData, unit: DurationUnitEnum): Promise<string> =>
   new Promise((resolve, reject) => {
-    const tableData = aggregatedToTableData({ data, unit });
-    const rows = tableData.rows
-      .map((row) =>
-        Objects.mapValues((v) => typeof v === 'object' ? v.content : v, row.columns));
+    const { rows } = calculateTotalsUsing(unit)(data);
 
     // TODO reorder columns to avoid depending on object property order
     csv.writeToString(rows, { headers: true }, (err, data) => err ? reject(err) : resolve(data));
