b0bb57d7d01373de09f99178269f86843354fe1a
AstroAsh <astroaisha@gmail.com>
2019-05-28 21:46:53 +0200

    ⚠️ wip
    ✅ add test
    
    🔥 remove test graph
    
    📊 move legend position
    
    💩 add temp graphs
    
    👌🏽 respond to comments

---
+++ dashboard-app/src/dashboard/dataManipulation/aggregatedToGraphData.ts
@@ -0,0 +1,40 @@
+import { AggregatedData } from './logsToAggregatedData';
+import { toUnitDuration } from './util';
+import { omit, map, Dictionary } from 'ramda';
+import { DurationUnitEnum } from '../../types';
+import { GraphColourList } from '../../styles/design_system';
+
+export const STACKED_TABLE_OPTIONS = {
+  legend: {
+    position: 'right',
+  },
+  scales: {
+    xAxes: [{
+      stacked: true,
+    }],
+    yAxes: [{
+      stacked: true,
+    }],
+  },
+};
+
+export const aggregatedToStackedGraph = (aggData: AggregatedData, unit: DurationUnitEnum) => {
+  const [groupBy, ...labels] = aggData.headers;
+  return {
+    labels,
+    datasets: aggData.rows.map((row, i) => {
+      const label = row[groupBy];
+      const rowData = omit([groupBy], row);
+      const numericData = map((v) =>
+        typeof v === 'object'
+          ? toUnitDuration(unit, v)
+          : v
+        , rowData) as Dictionary<any>;
+      return {
+        backgroundColor: GraphColourList[i],
+        label,
+        data: labels.map((y) => numericData[y]),
+      };
+    }),
+  };
+};
