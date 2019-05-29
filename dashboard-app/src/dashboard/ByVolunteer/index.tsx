a6c10dd475785ad9221b3698e88ccd2203fc6fa6
AstroAsh <astroaisha@gmail.com>
2019-05-29 18:09:13 +0200

    🔥 remove test graph

--- dashboard-app/src/dashboard/ByVolunteer/index.tsx
+++ dashboard-app/src/dashboard/ByVolunteer/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback, FunctionComponent } from 'reac
 import { withRouter, RouteComponentProps } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
-import { Bar } from 'react-chartjs-2';
 
 import DatePickerConstraints from './datePickerConstraints';
 import _DataTable from '../../components/DataTable';
@@ -151,13 +150,6 @@ const ByVolunteer: FunctionComponent<RouteComponentProps> = (props) => {
             )
           }
         </Col>
-      </Row>
-      <Row>
-        {aggData &&
-          (<Bar
-            data={aggregatedToStackedGraph(aggData, unit)}
-            options={STACKED_TABLE_OPTIONS}
-        />)}
       </Row>
     </Container>
   );
