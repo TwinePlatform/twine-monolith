216f727add786e43554e04ac3fea44d032ee3534
Elias Malik <elias0789@gmail.com>
2019-05-29 23:55:30 +0100

    fix bug by tracking sorted column by index instead of title
    also update tests

--- dashboard-app/src/dashboard/ByActivity/__tests__/index.test.ts
+++ dashboard-app/src/dashboard/ByActivity/__tests__/index.test.ts
@@ -1,18 +1,29 @@
-import { cleanup } from 'react-testing-library';
+import { cleanup, waitForElement } from 'react-testing-library';
+import MockAdapter from 'axios-mock-adapter';
 import { renderWithHistory } from '../../../util/tests';
+import { axios } from '../../../api';
 import ByActivity from '..';
 import 'jest-dom/extend-expect';
 
 describe('By Activity Page', () => {
-  beforeEach(cleanup);
+  let mock: MockAdapter;
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
 
   afterEach(cleanup);
 
   test('Success :: Display Title', async () => {
     expect.assertions(1);
+
+    mock.onGet('/community-businesses/me/volunteer-logs').reply(200, { result: [] });
+    mock.onGet('/community-businesses/me/volunteers').reply(200, { result: [] });
+    mock.onGet('/volunteer-activities').reply(200, { result: [] });
+
     const tools = renderWithHistory(ByActivity);
 
-    const title = await tools.getByText('By', { exact: false });
+    const title = await waitForElement(() => tools.getByText('By', { exact: false }));
 
     expect(title.textContent).toEqual('By Activity');
   });
--- dashboard-app/src/dashboard/ByTime/__tests__/index.test.ts
+++ dashboard-app/src/dashboard/ByTime/__tests__/index.test.ts
@@ -1,18 +1,27 @@
-import { cleanup } from 'react-testing-library';
+import { cleanup, waitForElement } from 'react-testing-library';
+import MockAdapter from 'axios-mock-adapter';
 import { renderWithHistory } from '../../../util/tests';
+import { axios } from '../../../api';
 import ByTime from '..';
 import 'jest-dom/extend-expect';
 
 describe('By Activity Page', () => {
-  beforeEach(cleanup);
+  let mock: MockAdapter;
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
 
   afterEach(cleanup);
 
   test('Success :: Display Title', async () => {
     expect.assertions(1);
+
+    mock.onGet('/community-businesses/me/volunteer-logs').reply(200, { result: [] });
+
     const tools = renderWithHistory(ByTime);
 
-    const title = await tools.getByText('By', { exact: false });
+    const title = await waitForElement(() => tools.getByText('By', { exact: false }));
 
     expect(title.textContent).toEqual('By Time');
   });
