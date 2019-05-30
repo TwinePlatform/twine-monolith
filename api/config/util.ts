d375ba1842cc4fc30f157f7ba12a293e05ffa88e
Elias Malik <elias0789@gmail.com>
2019-05-30 15:51:07 +0100

    🔧 make pool limit configurable via env var

--- api/config/util.ts
+++ api/config/util.ts
@@ -4,3 +4,8 @@ export const envListOr = (f: string, d: string[], sep: string = ',') => {
   const xs = (process.env[f] || '').split(sep);
   return xs.length > 0 && xs[0].length > 0 ? xs : d;
 };
+
+export const envNumberOr = (f: string, d: number) => {
+  const n = Number(process.env[f]);
+  return isNaN(n) ? d : n;
+};
