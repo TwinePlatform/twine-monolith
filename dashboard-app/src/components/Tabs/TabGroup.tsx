b3b8fae75fd235b963ef64e378f129177803ceaf
Elias <elias0789@gmail.com>
2019-05-30 15:42:23 +0100

    ✨ add tab components (#126)
    Fixes #121 
    
    ### Changes
    * Add `TabGroup` component (and sub-components)	based on designs
    * Add tests
    
    Usage example:
    ```
    <TabGroup titles={[TABLE_TITLE, 'Some other table']}>
      <DataTable
        {...tableData}
        title={TABLE_TITLE}
        sortBy={sortBy}
        initialOrder="desc"
        onChangeSortBy={onChangeSortBy}
        showTotals
      />
      <DataTable
        {...tableData}
        title={"Some other table"}
        sortBy={sortBy}
        initialOrder="desc"
        onChangeSortBy={onChangeSortBy}
        showTotals
      />
    </TabGroup>
    ```

---
+++ dashboard-app/src/components/Tabs/TabGroup.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import { TabList } from './TabList';
+import { Tab } from './Tab';
+
+
+/**
+ * Types
+ */
+type TabProps = {
+  titles: string[]
+  initialActiveTab?: number
+};
+
+/**
+ * Styles
+ */
+const TabContainer = styled.div``;
+
+export const TabGroup: React.FunctionComponent<TabProps> = (props) => {
+  const { initialActiveTab = 0 } = props;
+  const [activeTab, setActiveTab] = useState(initialActiveTab);
+
+  const onClickTab = useCallback(setActiveTab, []);
+
+  const children = React.Children.map(props.children, (child, idx) => (
+    idx === activeTab
+      ? (
+        <Tab isActive idx={idx}>
+          {child}
+        </Tab>
+      )
+      : null
+  ));
+
+  return (
+    <TabContainer>
+      <TabList
+        items={props.titles}
+        activeTab={activeTab}
+        onClickTab={onClickTab}
+      />
+      {children}
+    </TabContainer>
+  );
+};
---
+++ dashboard-app/src/components/Tabs/TabList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { rgba } from 'polished';
+import { TabListItem } from './TabListItem';
+import { ColoursEnum } from '../../styles/design_system';
+
+
+/**
+ * Types
+ */
+type TabListProps = {
+  items: string[]
+  activeTab: number
+  onClickTab?: (n: number) => void
+};
+
+
+/**
+ * Styles
+ */
+const List = styled.ol`
+  list-style: none;
+  display: flex;
+  border-bottom: 1px solid ${rgba(ColoursEnum.grey, 0.4)};
+`;
+
+export const TabList: React.FunctionComponent<TabListProps> = (props) => {
+  const {
+    items,
+    activeTab,
+    onClickTab = () => {},
+  } = props;
+
+  return (
+    <List>
+      {
+        items.map((item, idx) => (
+          <TabListItem
+            title={item}
+            isActive={idx === activeTab}
+            onClick={() => onClickTab(idx)}
+            key={item}
+          />
+        ))
+      }
+    </List>
+  );
+};
