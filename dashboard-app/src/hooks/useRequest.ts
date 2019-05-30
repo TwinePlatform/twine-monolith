93a83ce11f2bf0839ce35414ad54e06af9d67440
Elias Malik <elias0789@gmail.com>
2019-05-30 16:44:15 +0100

    make useRequest depend on react-use

--- dashboard-app/src/hooks/useRequest.ts
+++ dashboard-app/src/hooks/useRequest.ts
@@ -1,58 +1,19 @@
-import { useEffect, useState } from 'react';
-import { AxiosPromise, AxiosResponse, AxiosRequestConfig } from 'axios';
+import { DependencyList } from 'react';
+import { useAsyncFn } from 'react-use';
+import { AxiosRequestConfig } from 'axios';
+import { axios } from '../api';
 import { Dictionary } from 'ramda';
-import { Response } from '../util/response';
-import { redirectOnError } from '../util/routing';
 
-type RequestParams = {
-  apiCall: (params?: Pick<AxiosRequestConfig, 'params'>) => AxiosPromise
-  params?: any
-  updateOn?: any[]
-  callback?: (d: any) => void
-  setErrors: (d: any) => void
-  push: any
+
+export type UseRequestParams = {
+  config: AxiosRequestConfig
+  params: Dictionary<string | number>
+  updateOn: DependencyList
 };
 
-interface OnError {
-  (error: any,
-    setErrors: (d: Dictionary<string>) => void ,
-    push: (p: string) => void
-    ): void;
-}
 
-const onError: OnError = (error, setErrors, push) => {
-  const res = error.response;
-  if (Response.statusEquals(res, 400)) {
-    setErrors(Response.validationError(res));
-  } else {
-    redirectOnError(push, error);
-  }
+const useRequest = ({ config, updateOn }: UseRequestParams) => {
+  return useAsyncFn(() => axios(config), updateOn);
 };
 
-export default ({
-  apiCall,
-  params = {},
-  updateOn = [],
-  callback,
-  setErrors,
-  push,
-}: RequestParams) => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState();
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      try {
-        const res: AxiosResponse = await apiCall(params);
-        setData(Response.get(res));
-        if (callback) {
-          callback(Response.get(res));
-        }
-      } catch (error) {
-        onError(error, setErrors, push);
-      }
-      setLoading(false);
-    })();
-  }, updateOn);
-  return { loading, data };
-};
+export default useRequest;
