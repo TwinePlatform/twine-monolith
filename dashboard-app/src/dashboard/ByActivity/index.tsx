5f96ac0cdc7e05490b3207c66baf4bf26c3a222c
Elias Malik <elias0789@gmail.com>
2019-06-05 13:33:47 +0100

    fix activity page

--- dashboard-app/src/dashboard/ByActivity/index.tsx
+++ dashboard-app/src/dashboard/ByActivity/index.tsx
@@ -16,6 +16,7 @@ import { ColoursEnum } from '../../styles/design_system';
 import Errors from '../../components/Errors';
 import useAggregateDataByActivity from '../hooks/useAggregateDataByActivity';
 import { TabGroup } from '../../components/Tabs';
+import { Dictionary } from 'ramda';
 
 
 /**
@@ -54,7 +55,7 @@ const ByActivity: FunctionComponent<RouteComponentProps> = (props) => {
   const [fromDate, setFromDate] = useState<Date>(DatePickerConstraints.from.default());
   const [toDate, setToDate] = useState<Date>(DatePickerConstraints.to.default());
   const [tableData, setTableData] = useState<TableData>(initTableData);
-  const [errors, setErrors] = useState();
+  const [errors, setErrors] = useState<Dictionary<string>>({});
   const { loading, error, data } = useAggregateDataByActivity({ from: fromDate, to: toDate });
 
   useEffect(() => {
