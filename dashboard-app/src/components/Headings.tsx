c5bffd0e3adfc6e37ba01b861dc8e292786f0079
AstroAsh <astroaisha@gmail.com>
2019-06-06 14:53:17 +0200

    sneaky spacing

--- dashboard-app/src/components/Headings.tsx
+++ dashboard-app/src/components/Headings.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { ColoursEnum, Fonts } from '../styles/design_system';
 
-
 export const H1 = styled.h1`
   font-size: ${Fonts.size.heading1};
   font-weight: ${Fonts.weight.light};
@@ -24,7 +23,7 @@ export const H3 = styled.h3`
   color: ${ColoursEnum.black};
   text-align: center;
   margin-bottom: 1.5rem;
-  `;
+`;
 
 export const H4 = styled.h4`
   font-size: ${Fonts.size.heading4};
