55ec854472639c45fce2a6065c62f7b46df19fa3
AstroAsh <astroaisha@gmail.com>
2019-06-24 02:50:51 +0400

    📊 reverse order of stack in stacked bar chart (#186)
    - display colours in the same order as legend
    
    fixes #171

--- dashboard-app/src/dashboard/dataManipulation/__tests__/aggregatedToGraphData.test.ts
+++ dashboard-app/src/dashboard/dataManipulation/__tests__/aggregatedToGraphData.test.ts
@@ -28,16 +28,16 @@ describe('aggregatedToGraphData', () => {
     expect(expected).toEqual({
       datasets: [
         {
-          backgroundColor: '#F44336',
-          data: [0, 0.03, 0],
-          label: 'Digging Holes',
-          id: 3,
-        },
-        {
           backgroundColor: '#E91E63',
           data: [0, 2.38, 2],
           label: 'Outdoor and practical work',
           id: 5,
+        },
+        {
+          backgroundColor: '#F44336',
+          data: [0, 0.03, 0],
+          label: 'Digging Holes',
+          id: 3,
         },
       ],
       labels: [['Feb', '2018'], ['Mar', '2018'], ['Apr', '2018']],
