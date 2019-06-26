22ff2126ad1546746e2e9dbf78487dedd2612755
Elias Malik <elias0789@gmail.com>
2019-06-26 10:27:38 +0100

    increase padding

--- dashboard-app/src/components/Tabs/TabListItem.tsx
+++ dashboard-app/src/components/Tabs/TabListItem.tsx
@@ -17,7 +17,7 @@ type TabListItemProps = {
  * Styles
  */
 const Item = styled.li`
-  padding: 1rem;
+  padding: 1rem 2rem;
   cursor: pointer;
   ${
     (props: Pick<TabListItemProps, 'isActive'>) => props.isActive
