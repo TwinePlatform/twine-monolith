77c1a479b9c86b32c1560dd53a6f37e054bfdff4
Elias Malik <elias0789@gmail.com>
2019-07-02 15:26:02 +0100

    fix same defect on all pages

--- dashboard-app/src/features/dashboard/ByVolunteer/index.tsx
+++ dashboard-app/src/features/dashboard/ByVolunteer/index.tsx
@@ -59,6 +59,13 @@ const ByVolunteer: FunctionComponent<RouteComponentProps> = (props) => {
     if (idx > -1) {
       setSortBy(idx);
     }
+
+    // Requirement: If column being selected for sorting is "Volunteer Name"
+    //              (i.e. the first column), sort ascending (A-Z) instead of
+    //              descending (Z-A)
+    if (idx === 0) {
+      return 'asc';
+    }
   }, [tableData]);
 
   const downloadAsCsv = useCallback(() => {
