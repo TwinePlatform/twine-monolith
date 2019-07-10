387729cd86137e865a7b4fcb749a6c3e62ab4ada
AstroAsh <astroaisha@gmail.com>
2019-07-10 11:44:29 +0100

    👌🏽

--- dashboard-app/src/features/dashboard/components/DataCard/util.ts
+++ dashboard-app/src/features/dashboard/components/DataCard/util.ts
@@ -5,16 +5,16 @@ export const sentenceCreator = (xs: string[]) => {
     case 1:
       return xs;
     case 2:
-      return [xs[0], 'and', xs[1]].map((x) => x.replace('and', ' and '));
+      return [xs[0], ' and ', xs[1]];
     case 3:
     default:
-      const first = xs.length === 3
-        ? xs.slice(0, 1)
-        : xs.slice(0, 2);
-      const firstWithComma = first.join(' , ').split(' ');
-      const [penultimate, last] = xs.slice(-2);
+      const first = xs.slice(0, -1);
+      const firstWithComma = first
+        .reduce((a: string[], e: string) => a.concat(e, ', '), [])
+        .slice(0, -1);
+      const [last] = xs.slice(-1);
       return firstWithComma
-        .concat([',', penultimate, 'and', last])
-        .map(((x) => x.replace(',', ', ').replace('and', ' and ')));
+        .concat([' and ', last]);
+
   }
 };
