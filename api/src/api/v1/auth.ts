722b15198691ca181520ec797e38f5b2c675c35e
Elias Malik <elias0789@gmail.com>
2019-07-24 10:48:32 +0200

    wip

--- api/src/api/v1/auth.ts
+++ api/src/api/v1/auth.ts
@@ -2,8 +2,9 @@
  * Authentication Strategies
  */
 import * as Hapi from '@hapi/hapi';
-import * as AuthJwt from 'hapi-auth-jwt2';
-import * as standardStrategy from '../../auth/strategies/standard';
+import SessionCookieSchema from '../../auth/schema/session_cookie';
+import * as standardStrategy from '../../auth/strategies/new_standard';
+// import * as standardStrategy from '../../auth/strategies/standard';
 import * as externalStrategy from '../../auth/strategies/external';
 const AuthBearer = require('hapi-auth-bearer-token');
 
@@ -15,21 +16,17 @@ export default async (server: Hapi.Server) => {
    * Pre-requisite plugins
    */
   await server.register([
-    { plugin: AuthJwt, once: true },
+    { plugin: SessionCookieSchema, once: true, ...standard.session_cookie.options },
     { plugin: AuthBearer, once: true },
   ]);
 
   /*
    * Standard strategy
    */
-  server.auth.strategy('standard', 'jwt', {
-    key: standard.jwt.secret,
+  server.auth.strategy('standard', 'session_cookie', {
     validate: standardStrategy.validate,
-    verifyOptions: { algorithms: ['HS256'], ...standard.jwt.verifyOptions },
     cookieKey: standard.cookie.name,
   });
-
-  server.state(standard.cookie.name, standard.cookie.options);
 
   /*
    * External strategy
--- api/src/auth/strategies/new_standard/session.ts
+++ api/src/auth/strategies/new_standard/session.ts
@@ -3,4 +3,5 @@ import { Session } from './types';
 
 export const Sessions = {
   get: (req: Hapi.Request, sid: string): Session => req.yar.get(sid),
+  set: (req: Hapi.Request, key: string, value: any) => req.yar.set(key, value),
 };
