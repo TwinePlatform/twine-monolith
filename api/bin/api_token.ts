97355a2320527b7757383b2d7b5db39451fbe3f6
Elias Malik <elias0789@gmail.com>
2019-08-02 11:30:51 +0200

    add notes

--- api/bin/api_token.ts
+++ api/bin/api_token.ts
@@ -1,5 +1,15 @@
 /*
  * Command line utility for creating an API token
+ *
+ * Usage:
+ *  npm run exec ./bin/api_token.ts -- --cb=CB_NAME --scope=SCOPE
+ *
+ * Flags:
+ *  cb: Name of the CB for which to create the API token
+ *  scope: What scope to give the token (corresponds to api_token_access column)
+ *
+ * Note: Including the argument separator `--` before any of the flags is important.
+ *   Without it, the argument parsing will not work.
  */
 import * as parse from 'minimist';
 import * as Knex from 'knex';
