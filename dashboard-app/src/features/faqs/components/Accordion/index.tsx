f877c32e019a6e5aa8b7ab05e672f70c455e625d
Elias Malik <elias0789@gmail.com>
2019-08-13 11:00:50 +0100

    switch to react-collapsible with nicer animation natively

--- dashboard-app/src/features/faqs/components/Accordion/index.tsx
+++ dashboard-app/src/features/faqs/components/Accordion/index.tsx
@@ -1,15 +1,6 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
-import { Row } from 'react-flexbox-grid';
-import {
-  Accordion as AccordionComponent,
-  AccordionItem,
-  AccordionItemButton,
-  AccordionItemHeading,
-  AccordionItemPanel,
-  AccordionItemState,
-} from 'react-accessible-accordion';
 import { Paragraph } from '../../../../lib/ui/components/Typography';
+import Collapsible from 'react-collapsible';
 
 import './styles.css';
 
@@ -21,37 +12,21 @@ type AccordionProps = {
   }[]
 };
 
-const PanelItem = styled(Paragraph)`
-  line-height: 1.3;
-  margin-bottom: 2em;
-`;
 
 export const Accordion: FunctionComponent<AccordionProps> = ({ panels = [] }) => (
-  <AccordionComponent>
+  <div>
     {
       panels.map(({ heading, contents }) =>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton>
-              {heading}
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel>
-            <AccordionItemState>
-              {
-                (state: object) =>
-                  contents.map((content) =>
-                    <Row start="xs">
-                      <PanelItem>
-                        {content}
-                      </PanelItem>
-                    </Row>
-                  )
-              }
-            </AccordionItemState>
-          </AccordionItemPanel>
-        </AccordionItem>
+        <Collapsible trigger={heading}>
+          {
+            contents.map((content) =>
+              <Paragraph>
+                {content}
+              </Paragraph>
+            )
+          }
+        </Collapsible>
       )
     }
-  </AccordionComponent>
+  </div>
 );
