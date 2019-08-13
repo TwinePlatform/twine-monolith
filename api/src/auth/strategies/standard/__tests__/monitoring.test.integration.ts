5cbfbb467f2657eefdc2bebf0667070034a1d5c0
Elias Malik <elias0789@gmail.com>
2019-08-13 13:18:21 +0100

    wip

--- api/src/auth/strategies/standard/__tests__/monitoring.test.integration.ts
+++ api/src/auth/strategies/standard/__tests__/monitoring.test.integration.ts
@@ -24,12 +24,26 @@ describe('REDIS', () => {
 
     const listener = createListener(knex, config.cache.session.options.url);
 
+    // const listener = new Redis(config.cache.session.options.url);
+
+    // listener.psubscribe('__keyevent@*__:expired', <any> ((...args: any[]) => {
+    //   console.log('SUBBED', ...args);
+    // }));
+
+    // listener.psubscribe('*', <any> ((...args: any[]) => {
+    //   console.log('SUBBED', ...args);
+    // }));
+
+    // listener.on('pmessage', <any> ((...args: any[]) => {
+    //   console.log('MESSAGE', ...args);
+    // }));
+
     await delay(1);
 
     try {
-      await client.set('foo', 1);
-      await client.pexpire('foo', 100);
-      await delay(500);
+      await client.set('foo', 1, 'px', 1);
+      // await client.pexpire('foo', 1);
+      await delay(10000);
 
     } catch (error) {
       console.log(error);
@@ -38,24 +52,24 @@ describe('REDIS', () => {
 
     const results = await knex('user_session_record').select('*');
 
-    expect(results).toHaveLength(1);
-    expect(results).toEqual([
-      {
-        created_at: new Date('2019-08-12T13:30:04.104Z'),
-        deleted_at: null,
-        ended_at: new Date('2019-08-12T13:30:10.102Z'),
-        modified_at: null,
-        organisation_id: 1,
-        referrers: [],
-        session_end_type: 'expired',
-        session_id: 'foo',
-        started_at: new Date('2019-08-12T13:30:04.101Z'),
-        user_account_id: 1,
-        user_session_record_id: 1,
-      },
-    ]);
-
     await listener.quit();
     await client.quit();
+
+    // expect(results).toHaveLength(1);
+    // expect(results).toEqual([
+    //   {
+    //     created_at: new Date('2019-08-12T13:30:04.104Z'),
+    //     deleted_at: null,
+    //     ended_at: new Date('2019-08-12T13:30:10.102Z'),
+    //     modified_at: null,
+    //     organisation_id: 1,
+    //     referrers: [],
+    //     session_end_type: 'expired',
+    //     session_id: 'foo',
+    //     started_at: new Date('2019-08-12T13:30:04.101Z'),
+    //     user_account_id: 1,
+    //     user_session_record_id: 1,
+    //   },
+    // ]);
   });
 });
--- api/src/auth/strategies/standard/monitoring.ts
+++ api/src/auth/strategies/standard/monitoring.ts
@@ -6,16 +6,17 @@ import { UserSessionRecords } from '../../../models/user_session_record';
 export const createListener = (knex: Knex, url: string) => {
   const client = new IoRedis(url);
 
-  client.subscribe('__keyevent@0__:expired');
+  client.psubscribe('__keyevent@*__:expired', <any> ((...args: any[]) => {
+    console.log('SUBBED', ...args);
+  }));
 
-  client.on('message', <any> (async (...args: any[]) => {
+  client.on('pmessage', <any> (async (...args: any[]) => {
     console.log('MESSAGE', ...args);
 
     switch (args[0]) {
-      case '__keyevent@0__:expired':
-        const x = await UserSessionRecords.endSession(knex, args[1], 'expired');
+      case '__keyevent@*__:expired':
+        const x = await UserSessionRecords.endSession(knex, args[2], 'expired');
         console.log('RECORDED', x);
-
         break;
 
       default:
