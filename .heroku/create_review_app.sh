228caedfe77fccaeeb7a798089a2fb5060b37594
Elias <elias0789@gmail.com>
2019-08-13 15:25:27 +0100

    fix argument parsing (#265)
    - forgot to `SHIFT` when parsing arguments
    - properly reference variables 🙈

--- .heroku/create_review_app.sh
+++ .heroku/create_review_app.sh
@@ -3,10 +3,10 @@
 # Create a heroku app from command line
 #
 # Usage:
-#  $ bash create_app.sh <PR> -avd
+#  $ bash create_app.sh -avd <PR>
 #
 # Example:
-#  $ bash create_app.sh 242 -vd
+#  $ bash create_app.sh -vd 242
 #
 # Arguments:
 #  PR - Number of the PR that this set of apps should be created for (e.g. 132)
@@ -44,7 +44,17 @@ function _create_app () {
 }
 
 
-# Main
+# Parse Arguments
+
+while getopts avd opt; do
+  case $opt in
+    a) CREATE_API="true";;
+    v) CREATE_VISITOR="true";;
+    d) CREATE_DASHBOARD="true";;
+    *) terminate "Unrecognised option $opt: only a, v, d supported"
+  esac
+done
+shift $(( OPTIND - 1 )) # Ignore flag arguments from $N
 
 PR=$1
 
@@ -60,23 +70,20 @@ if [ $(heroku auth:whoami) != "powertochangetwine@gmail.com" ]; then
   terminate "Must be logged in as pipeline owner"
 fi
 
-while getopts avd opt; do
-  case $opt in
-    a) CREATE_API="true";;
-    v) CREATE_VISITOR="true";;
-    d) CREATE_DASHBOARD="true";;
-    *) terminate "Unrecognised option $opt: only a, v, d supported"
-  esac
-done
 
-if [ CREATE_API == "true" ]; then
+# Main
+
+if [ $CREATE_API == "true" ]; then
+  echo "Creating review API app twine-api-pr-$PR"
   _create_app "twine-api-pr-$PR" "twine-api-staging" "twine-api-pipeline"
 fi
 
-if [ CREATE_VISITOR == "true" ]; then
+if [ $CREATE_VISITOR == "true" ]; then
+  echo "Creating review API app twine-visitor-pr-$PR"
   _create_app "twine-visitor-pr-$PR" "twine-visitor-staging" "twine-visitor-pipeline" "twine-api-pr-$PR"
 fi
 
-if [ CREATE_DASHBOARD == "true" ]; then
+if [ $CREATE_DASHBOARD == "true" ]; then
+  echo "Creating review API app twine-dashboard-pr-$PR"
   _create_app "twine-dashboard-pr-$PR" "twine-dashboard-staging" "twine-volunteer-dashboard" "twine-api-pr-$PR"
 fi
