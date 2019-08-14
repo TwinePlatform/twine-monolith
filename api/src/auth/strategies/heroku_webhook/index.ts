63f20a4121e3e2b8a0e6b2d52c54f7851cc7930c
AstroAsh <astroaisha@gmail.com>
2019-08-14 15:54:09 +0100

    👣

--- api/src/auth/strategies/heroku_webhook/index.ts
+++ api/src/auth/strategies/heroku_webhook/index.ts
@@ -1,13 +1,22 @@
 import * as Hapi from '@hapi/hapi';
 import * as Boom from '@hapi/boom';
+import { createHmac } from 'crypto';
 
 const validateHerokuWebhook =
   async (request: Hapi.Request, token: string, h: Hapi.ResponseToolkit) => {
     const { server: { app: { config } } } = request;
+    const error = Boom.forbidden('Error with authentication for 3rd party clients');
 
-    return config.webhooks.heroku.secret === token
-    ? { isValid: true, credentials: { user: 'heroku' } }
-    : Boom.forbidden('Error with authentication for 3rd party clients');
+    if (config.webhooks.heroku.authToken !== token) {
+      return error;
+    }
+    const signature = request.headers['Heroku-Webhook-Hmac-SHA256'];
+    const hmac = createHmac('sha256', config.webhooks.heroku.secret);
+    const hashedPayload = hmac.update(JSON.stringify(request.payload)).digest('hex');
+
+    return signature === hashedPayload
+      ? { isValid: true, credentials: { user: 'heroku' } }
+      : error;
   };
 
 export default validateHerokuWebhook;
