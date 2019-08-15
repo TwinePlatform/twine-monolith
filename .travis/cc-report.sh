628ad14265f09a17305d415ac4c271e18ba8bdb3
Elias Malik <elias0789@gmail.com>
2019-08-15 18:53:15 +0100

    don't use relative paths

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -f ./cc-test-reporter ]; then
+if [ ! -f $(dirname $0)/cc-test-reporter ]; then
   echo "No test reporter found";
   exit 1;
 fi
@@ -14,7 +14,7 @@ function format_lcov () {
   for f in $@; do
     if [ -f $1 ]; then
       echo "Formatting $f";
-      ./cc-test-reporter format-coverage -t lcov -o coverage/coverage.${1//\//-}.json $1;
+      $(dirname $0)/cc-test-reporter format-coverage -t lcov -o coverage/coverage.${1//\//-}.json $1;
     fi
   done
 }
@@ -23,7 +23,7 @@ files=$(find . -name 'lcov.info' -type f -not -path "*/node_modules/*")
 format_lcov ${files[@]}
 
 echo "Summing coverage reports"
-./cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage.*.json;
+$(dirname $0)/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage.*.json;
 
 echo "Uploading overall coverage report"
-./cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+$(dirname $0)/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
