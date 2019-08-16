c6667a65ff8514f8d605855afe34c830a9038fcb
Elias <elias0789@gmail.com>
2019-08-16 13:12:53 +0100

    🔧 travis/code-climate fix (#277)
    fixes #276
    
    ### Changes
    - Use absolute paths using `dirname` function instead of relying on `.`
    
    ### Testing Requirements
    - [ ] Travis / Github
      - Check that code-coverage reports have been uploaded
    
    ### Release Requirements
    N/A
    
    ### Manual Deployment
    N/A

--- .travis/cc-download.sh
+++ .travis/cc-download.sh
@@ -6,13 +6,13 @@ PLATFORM=$(uname -s)
 
 if [ $PLATFORM == "Darwin" ]; then
 
-  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64 > ./cc-test-reporter
-  chmod +x ./cc-test-reporter
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64 > $(dirname $0)/cc-test-reporter
+  chmod +x $(dirname $0)/cc-test-reporter
 
 elif [ $PLATFORM == 'Linux' ]; then
 
-  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-  chmod +x ./cc-test-reporter
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > $(dirname $0)/cc-test-reporter
+  chmod +x $(dirname $0)/cc-test-reporter
 
 else
 
