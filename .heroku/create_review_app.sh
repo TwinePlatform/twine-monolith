ff741a580453f7627c9b91b3f829f8ea4ed568f4
Elias Malik <elias0789@gmail.com>
2019-08-22 12:06:58 +0100

    update script

--- .heroku/create_review_app.sh
+++ .heroku/create_review_app.sh
@@ -3,13 +3,14 @@
 # Create a heroku app from command line
 #
 # Usage:
-#  $ bash create_app.sh -avd <PR>
+#  $ bash create_app.sh -avd <PR> <BRANCH>
 #
 # Example:
-#  $ bash create_app.sh -vd 242
+#  $ bash create_app.sh -vd 242 dash/create-widget
 #
 # Arguments:
-#  PR - Number of the PR that this set of apps should be created for (e.g. 132)
+#  PR     - Number of the PR that this set of apps should be created for (e.g. 132)
+#  BRANCH - Branch from which apps should be deployed
 #
 # Flags
 #  a - Setup a new review version of the API
@@ -24,11 +25,28 @@ function terminate () {
   exit 1
 }
 
+function _deploy_branch () {
+  local BRANCH=$1
+  local APP_NAME=$2
+
+  if [ ! -z $BRANCH ]; then
+    REMOTE_EXISTS=$(git remote -v | grep "git.heroku.com/$APP_NAME.git")
+    if [ -z $REMOTE_EXISTS ]; then
+      git remote add heroku-$APP_NAME https://git.heroku.com/$APP_NAME.git
+    fi
+    git pull origin $BRANCH
+    git push heroku $BRANCH:master
+    git branch -D $BRANCH
+    git remote rm heroku-$APP_NAME
+  fi
+}
+
 function _create_app () {
   local APP_NAME=$1
   local TEMPLATE_APP_NAME=$2
   local PIPELINE_NAME=$3
   local API_APP=$4
+  local BRANCH=$5
 
   heroku apps:create $APP_NAME --region eu --buildpack https://github.com/TwinePlatform/heroku-buildpack-select-subdir
 
@@ -41,6 +59,8 @@ function _create_app () {
   if [ ! -z $API_APP ]; then
     heroku config:set PROXY_API_URL="https://$API_APP.herokuapp.com" -a $APP_NAME
   fi
+
+  _deploy_branch $BRANCH $APP_NAME
 }
 
 
@@ -57,6 +77,7 @@ done
 shift $(( OPTIND - 1 )) # Ignore flag arguments from $N
 
 PR=$1
+BRANCH=$2
 
 if [ -z $PR ]; then
   terminate "Must provide a name for the app"
@@ -73,17 +94,20 @@ fi
 
 # Main
 
-if [ $CREATE_API == "true" ]; then
+API_TARGET="twine-api-staging"
+
+if [ $CREATE_API = "true" ]; then
   echo "Creating review API app twine-api-pr-$PR"
-  _create_app "twine-api-pr-$PR" "twine-api-staging" "twine-api-pipeline"
+  _create_app "twine-api-pr-$PR" "twine-api-staging" "twine-api-pipeline" "" $BRANCH
+  API_TARGET="twine-api-pr-$PR"
 fi
 
-if [ $CREATE_VISITOR == "true" ]; then
+if [ $CREATE_VISITOR = "true" ]; then
   echo "Creating review API app twine-visitor-pr-$PR"
-  _create_app "twine-visitor-pr-$PR" "twine-visitor-staging" "twine-visitor-pipeline" "twine-api-pr-$PR"
+  _create_app "twine-visitor-pr-$PR" "twine-visitor-staging" "twine-visitor-pipeline" $API_TARGET $BRANCH
 fi
 
-if [ $CREATE_DASHBOARD == "true" ]; then
+if [ $CREATE_DASHBOARD = "true" ]; then
   echo "Creating review API app twine-dashboard-pr-$PR"
-  _create_app "twine-dashboard-pr-$PR" "twine-dashboard-staging" "twine-volunteer-dashboard" "twine-api-pr-$PR"
+  _create_app "twine-dashboard-pr-$PR" "twine-dashboard-staging" "twine-volunteer-dashboard" $API_TARGET $BRANCH
 fi
