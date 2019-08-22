aa7e10411880b0d99f27038b7435cb0135d90323
AstroAsh <astroaisha@gmail.com>
2019-08-22 17:40:09 +0100

    🐛 export release ribbon

--- lib/twine-ui/src/components/index.tsx
+++ lib/twine-ui/src/components/index.tsx
@@ -3,6 +3,7 @@ import Input from './Input';
 import LabelledSelect, { Select, SelectWrapper, Option } from './Select';
 import { H1, H2, H3, H4, Text, Span, Special } from './Typography';
 import { Button, BigButton, CancelButton } from './Buttons';
+import { ReleaseRibbon } from './ReleaseRibbon/index'
 
 
 export {
@@ -22,4 +23,5 @@ export {
   Button,
   BigButton,
   CancelButton,
+  ReleaseRibbon,
 }
