613a0d52082aea997a75ffa633711149b8657665
Elias Malik <elias0789@gmail.com>
2019-08-23 15:47:13 +0100

    fix import

--- lib/twine-ui/src/components/index.tsx
+++ lib/twine-ui/src/components/index.tsx
@@ -3,7 +3,7 @@ import Input from './Input';
 import LabelledSelect, { Select, SelectWrapper, Option } from './Select';
 import { H1, H2, H3, H4, Text, Span, Special } from './Typography';
 import { Button, BigButton, CancelButton } from './Buttons';
-import { ReleaseRibbon } from './ReleaseRibbon/index'
+import ReleaseRibbon from './ReleaseRibbon/ReleaseRibbon';
 
 
 export {
