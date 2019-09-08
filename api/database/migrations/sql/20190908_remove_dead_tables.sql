5bb5307880b3d2917fcc77931ca356cfaf1201d6
Elias Malik <elias0789@gmail.com>
2019-09-08 22:13:12 +0100

    remove dead tables

---
+++ api/database/migrations/sql/20190908_remove_dead_tables.sql
@@ -0,0 +1,14 @@
+/*
+ * Remove tables that are unused
+ */
+DROP TABLE data_sync_log;
+DROP TABLE funding_body;
+DROP TABLE funding_programme;
+DROP TABLE funding_programme_community_business;
+DROP TABLE outreach_campaign;
+DROP TABLE outreach_campaign_target;
+DROP TABLE outreach_campaign_target_type;
+DROP TABLE outreach_meeting;
+DROP TABLE outreach_meeting_type;
+DROP TABLE outreach_type;
+DROP TABLE training_session;
