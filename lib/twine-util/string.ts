ea53f944b61b82973bca90550d28a82db82dba12
Elias Malik <elias0789@gmail.com>
2019-09-12 14:01:39 +0100

    switch twine-util to eslint from tslint

--- lib/twine-util/string.ts
+++ lib/twine-util/string.ts
@@ -19,16 +19,16 @@ export const listify = (xs: string[], opts: { and?: boolean } = { and: true }) =
   }
 
   switch (xs.length) {
-    case 0:
-    case 1:
-      return xs;
-    case 2:
-      return intersperse(' and ', xs);
-    case 3:
-    default:
-      return intersperse(', ', xs.slice(0, -1))
-        .concat(' and ')
-        .concat(xs.slice(-1));
+  case 0:
+  case 1:
+    return xs;
+  case 2:
+    return intersperse(' and ', xs);
+  case 3:
+  default:
+    return intersperse(', ', xs.slice(0, -1))
+      .concat(' and ')
+      .concat(xs.slice(-1));
   }
 };
 
