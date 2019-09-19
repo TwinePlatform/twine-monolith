b89801b34e97a263041ac36453d85e1e91bd6ed3
AstroAsh <astroaisha@gmail.com>
2019-09-19 18:05:57 +0200

    💅 add styled components

--- nativeApp/App.tsx
+++ nativeApp/App.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native'
+
 import { CommunityBusinesses } from './src/lib/api';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
+const View = styled.View`
+  alignItems: center;
+  justifyContent: center;
+  flex: 1;
+`;
+
+const Text = styled.Text`
+  font-size: 15;
+`;
 
 export default function App() {
   const [data, setData] = useState();
@@ -26,7 +28,7 @@ export default function App() {
     )},[]);
     
   return (
-    <View style={styles.container}>
+    <View>
       {data && data.map(x => <Text key={x.id}>{x.name}</Text>)}
       {error && <Text>{JSON.stringify(error)}</Text>}
     </View>
