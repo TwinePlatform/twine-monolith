7608b2f1e9bbae9cde9fc8a236ce50f62da2b26d
Elias Malik <elias0789@gmail.com>
2019-09-23 15:12:31 +0100

    diagnostic

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -25,5 +25,7 @@ format_lcov ${files[@]}
 echo "Summing coverage reports"
 $DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
 
+echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
+
 echo "Uploading overall coverage report"
 $DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
