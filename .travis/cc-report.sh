d69a61c6a09467698260222900220cf70e7bc91e
Elias Malik <elias0789@gmail.com>
2019-09-23 12:56:34 +0100

    fix filename processing

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -7,9 +7,11 @@ fi
 
 function format_lcov () {
   for f in $@; do
-    if [ -f $1 ]; then
-      echo "Formatting $f";
-      $(dirname $0)/cc-test-reporter format-coverage -t lcov -o coverage/coverage.${1//\//-}.json $1;
+    if [ -f $f ]; then
+      FILENAME=$(echo "$f" | cut -c2- | sed s_\/_-_g | sed s/.info/.json/)
+
+      echo "Formatting $f into coverage/coverage${FILENAME}";
+      $(dirname $0)/cc-test-reporter format-coverage -t lcov -o coverage/coverage${FILENAME} $1;
     fi
   done
 }
@@ -18,7 +20,7 @@ files=$(find . -name 'lcov.info' -type f -not -path "*/node_modules/*")
 format_lcov ${files[@]}
 
 echo "Summing coverage reports"
-$(dirname $0)/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage.*.json;
+$(dirname $0)/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage-*.json;
 
 echo "Uploading overall coverage report"
 $(dirname $0)/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
