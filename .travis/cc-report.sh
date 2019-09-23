3b95535b39d67d30eb306fece68a7e780b308b98
Elias Malik <elias0789@gmail.com>
2019-09-23 13:27:49 +0100

    try setting root dir

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -f $(dirname $0)/cc-test-reporter ]; then
+DIR=$(dirname $0)
+ROOT=$(dirname $DIR)
+
+if [ ! -f "$DIR/cc-test-reporter" ]; then
   echo "No test reporter found";
   exit 1;
 fi
@@ -8,19 +11,19 @@ fi
 function format_lcov () {
   for f in $@; do
     if [ -f $f ]; then
-      FILENAME=$(echo "$f" | cut -c2- | sed s_\/_-_g | sed s/.info/.json/)
+      FILENAME=$(echo "$f" | cut -c3- | sed s_\/_-_g | sed s/.info/.json/)
 
-      echo "Formatting $f into coverage/coverage${FILENAME}";
-      $(dirname $0)/cc-test-reporter format-coverage -t lcov -o coverage/coverage${FILENAME} $1;
+      echo "Formatting $f into coverage/coverage/${FILENAME}";
+      $DIR/cc-test-reporter format-coverage -t lcov -o coverage/coverage/${FILENAME} $1;
     fi
   done
 }
 
-files=$(find . -name 'lcov.info' -type f -not -path "*/node_modules/*")
+files=$(find "$ROOT" -name 'lcov.info' -type f -not -path "*/node_modules/*")
 format_lcov ${files[@]}
 
 echo "Summing coverage reports"
-$(dirname $0)/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage-*.json;
+$DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage-*.json;
 
 echo "Uploading overall coverage report"
-$(dirname $0)/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+$DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
