cbc8b95e13a493a16965faff4c255382a5ea45bd
Elias Malik <elias0789@gmail.com>
2019-09-25 17:21:52 +0100

    wip

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -28,12 +28,12 @@ echo "$files"
 
 format_lcov ${files[@]}
 
-echo "Summing coverage reports"
-$DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
+# echo "Summing coverage reports"
+# $DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
 
-echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
-echo "Contents:"
-echo $(cat coverage/coverage.total.json | head -n 100)
+# echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
+# echo "Contents:"
+# echo $(cat coverage/coverage.total.json | head -n 100)
 
-echo "Uploading overall coverage report"
-$DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+# echo "Uploading overall coverage report"
+# $DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
---
+++ .travis/cc-upload.sh
@@ -0,0 +1,10 @@
+
+echo "Summing coverage reports"
+$DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
+
+echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
+echo "Contents:"
+echo $(cat coverage/coverage.total.json | head -n 100)
+
+echo "Uploading overall coverage report"
+$DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
