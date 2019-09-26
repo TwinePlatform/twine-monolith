1fc6adaf639e5daf4517974369d0f3d14159254f
Elias Malik <elias0789@gmail.com>
2019-09-26 18:50:15 +0100

    update paths

--- .travis/cc-upload.sh
+++ .travis/cc-upload.sh
@@ -1,11 +1,14 @@
 DIR=$(dirname $0)
 
-echo "Summing coverage reports"
-$DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
+echo "Found coverage reports:"
+for f in coverage/* do; echo $f; done
 
-echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
+echo "Summing coverage reports"
+$DIR/cc-test-reporter sum-coverage -o ./coverage.total.json coverage/*;
+
+echo "Overall Result: $(cat ./coverage.total.json | grep covered_percent | head -n 1)"
 echo "Contents:"
-echo $(cat coverage/coverage.total.json | head -n 100)
+echo $(cat ./coverage.total.json | head -n 100)
 
 echo "Uploading overall coverage report"
-$DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+$DIR/cc-test-reporter upload-coverage -i ./coverage.total.json;
