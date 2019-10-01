a5d7fe8f52547bfcfb0de8d3e7e3591cbce7a8fc
AstroAsh <astroaisha@gmail.com>
2019-10-01 16:25:20 +0100

    🔧 add script for creating new component file

---
+++ nativeApp/bin/touch_tsx/index.ts
@@ -0,0 +1,44 @@
+/*
+ * Command line utility for a creating a blank template for components
+ *
+ * Usage:
+ *  npm run touch:tsx [PATH] [FILE (optional)]
+ * 
+ * If no file is supplied:
+ * - directory is created (if does not exist)
+ * - index file is created inside directory
+ * 
+ * If file is supplied
+ * - file is created inside of directory
+ *
+ * args:
+ *  PATH: path from root where the folder should be created
+ *  eg. ./src/open_views/screens/ForgotPassword
+ * 
+ * FILE: capitalised component name
+ *
+ */
+import getTemplate from './template';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+
+const capitaliseFirst = (string) => string.charAt(0).toUpperCase() + string.substring(1);
+
+(() => {
+
+  process.on('unhandledRejection', (err) => { throw err; });
+  
+  const [path, file] = process.argv.slice(2);
+  if (file) {
+    
+    const fileName = capitaliseFirst(file);
+    writeFileSync(`${path}/${fileName}.tsx`, getTemplate(fileName), { flag: 'wx' });
+    
+  } else {
+    const fileName = capitaliseFirst(path.split('/').slice(-1)[0]);
+
+    !existsSync(path) && mkdirSync(`${path}`);
+    writeFileSync(`${path}/index.tsx`, getTemplate(fileName), { flag: 'wx' });
+  }
+  
+  console.log('🎉 File has been created')
+})()
---
+++ nativeApp/bin/touch_tsx/template.ts
@@ -0,0 +1,31 @@
+export default (fileName) => {
+  return `
+import React, { FC } from 'react';
+import styled from 'styled-components/native'
+
+/*
+ * Types
+ */
+type Props = {
+}
+
+/*
+ * Styles
+ */
+const View = styled.View\`
+  \`;
+
+/*
+ * Component
+ */
+const ${fileName}: FC<Props> = (props) => {
+  return (
+    <View>
+    </View>
+  );
+};
+
+export default ${fileName};
+
+`
+}
