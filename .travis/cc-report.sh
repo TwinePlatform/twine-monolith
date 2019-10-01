544267d2c052e6d21248c80c5920b9eef59f5ec6
Elias Malik <elias0789@gmail.com>
2019-10-01 13:13:07 +0100

    fix aws install and upload cmds

--- .travis/cc-report.sh
+++ .travis/cc-report.sh
@@ -3,10 +3,6 @@
 DIR=$(dirname $0)
 ROOT=$(dirname $DIR)
 
-if [ ! -z $1 ]; then
-  ROOT=$1
-fi
-
 if [ ! -f "$DIR/cc-test-reporter" ]; then
   echo "No test reporter found";
   exit 1;
@@ -18,10 +14,10 @@ function format_lcov () {
       FILENAME=$(echo "$f" | cut -c3- | sed s_\/_-_g | sed s/.info/.json/)
 
       echo "Formatting $f into coverage/coverage-${FILENAME}";
-      $DIR/cc-test-reporter format-coverage -t lcov -o coverage/coverage-${FILENAME} $f;
+      $DIR/cc-test-reporter format-coverage -t lcov -o $ROOT/coverage/fmt-coverage-${FILENAME} $f;
 
       echo "Contents:"
-      echo $(cat coverage/coverage-${FILENAME} | head -n 100)
+      echo $(cat $ROOT/coverage/fmt-coverage-${FILENAME} | head -n 100)
     fi
   done
 }
@@ -31,13 +27,3 @@ echo "Found coverage info in:"
 echo "$files"
 
 format_lcov ${files[@]}
-
-# echo "Summing coverage reports"
-# $DIR/cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/*coverage-*.json;
-
-# echo "Overall Result: $(cat coverage/coverage.total.json | grep covered_percent | head -n 1)"
-# echo "Contents:"
-# echo $(cat coverage/coverage.total.json | head -n 100)
-
-# echo "Uploading overall coverage report"
-# $DIR/cc-test-reporter upload-coverage -i coverage/coverage.total.json;
