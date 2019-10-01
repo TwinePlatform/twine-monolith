97f7bf4381fdf59832e0cc61b7d8bb8129a33d34
AstroAsh <astroaisha@gmail.com>
2019-10-01 16:55:12 +0100

    🐪

--- nativeApp/src/lib/ui/typography.tsx
+++ nativeApp/src/lib/ui/typography.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components/native'
 
 export const Heading = styled.Text`
-  font-size: 30;
+  fontSize: 30;
 `;
 
 export const Heading2 = styled.Text`
-  font-size: 25;
+  fontSize: 25;
 `;
 
 export const Heading3 = styled.Text`
-  font-size: 20;
+  fontSize: 20;
 `;
 
 export enum FontsEnum {
