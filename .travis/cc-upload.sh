0f70ba7f8a851978a87e655095d0cc8bcdf58588
Elias Malik <elias0789@gmail.com>
2019-10-02 12:38:44 +0100

    shrug

--- .travis/cc-upload.sh
+++ .travis/cc-upload.sh
@@ -2,10 +2,10 @@ DIR=$(dirname $0)
 ROOT=$(dirname $DIR)
 
 echo "Found coverage reports:"
-for f in $ROOT/coverage/fmt-coverage-*; do echo $f; done
+for f in $ROOT/coverage/*; do echo $f; done
 
 echo "Summing coverage reports"
-$DIR/cc-test-reporter sum-coverage -o ./coverage.total.json $ROOT/coverage/fmt-coverage-*;
+$DIR/cc-test-reporter sum-coverage -o ./coverage.total.json $ROOT/coverage/*;
 
 echo "Overall Result: $(cat ./coverage.total.json | grep covered_percent | head -n 1)"
 echo "Contents:"
