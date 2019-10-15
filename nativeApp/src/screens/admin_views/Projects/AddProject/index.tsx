4ab3ed8388a32213178292f83408815e4bdc653a
AstroAsh <astroaisha@gmail.com>
2019-10-15 14:44:02 +0100

    ✨ setup add project page

--- nativeApp/src/screens/admin_views/Projects/AddProject/index.tsx
+++ nativeApp/src/screens/admin_views/Projects/AddProject/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 // import styled from 'styled-components/native';
 import Page from '../../../../lib/ui/Page';
+import { Heading } from '../../../../lib/ui/typography';
+import Input from '../../../../lib/ui/forms/InputWithInlineLabel';
+import SubmitButton from '../../../../lib/ui/forms/SubmitButton';
 
 /*
  * Types
@@ -17,7 +20,9 @@ type Props = {
  */
 const AddProject: FC<Props> = () => (
   <Page>
-    {}
+    <Heading>Add Project</Heading>
+    <Input label="Name" />
+    <SubmitButton text="Add Project" onPress={() => {}} />
   </Page>
 );
 
