d8a3e85739b04c453c6caea5d0bbe4a6ebc7af64
AstroAsh <astroaisha@gmail.com>
2019-10-18 17:36:24 +0100

    ✨ create AddBar component and implement for volunteers

---
+++ nativeApp/src/lib/ui/AddBar.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import styled from 'styled-components/native';
+import { ColoursEnum } from './colours';
+import { FontsEnum } from './typography';
+
+/*
+ * Types
+ */
+type Props = {
+  onPress: () => void;
+  title: string;
+}
+
+/*
+ * Styles
+ */
+const Container = styled.View`
+  width: 85%;
+  justifyContent: flex-end;
+  alignItems: flex-end;
+`;
+
+const Button = styled.TouchableOpacity`
+  paddingTop: 0;
+  paddingBottom: 10;
+  // marginTop:0;
+`;
+
+const Text = styled.Text`
+  color: ${ColoursEnum.purple};
+  fontFamily: ${FontsEnum.regular};
+  fontSize: 16;
+`;
+/*
+ * Component
+ */
+const AddBar: FC<Props> = ({ onPress, title }) => (
+  <Container>
+    <Button onPress={onPress}>
+      <Text>{title}</Text>
+    </Button>
+  </Container>
+);
+
+export default AddBar;
--- nativeApp/src/screens/admin_views/Volunteers/VolunteersPage/index.tsx
+++ nativeApp/src/screens/admin_views/Volunteers/VolunteersPage/index.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 // import styled from 'styled-components/native';
+
+import { NavigationFocusInjectedProps } from 'react-navigation';
 import VolunteerCard from './VolunteerCard';
 import Page from '../../../../lib/ui/Page';
+import AddBar from '../../../../lib/ui/AddBar';
 
 /*
  * Types
@@ -18,8 +21,9 @@ type Props = {
 */
 
 
-const Volunteers: FC<Props> = () => (
+const Volunteers: FC<NavigationFocusInjectedProps & Props> = ({ navigation }) => (
   <Page heading="Volunteers">
+    <AddBar title="Add Volunteer" onPress={() => navigation.navigate('AdminAddVolunteer')} />
     <VolunteerCard id={1} title="Kara Thrace" date="11/11/18" />
     <VolunteerCard id={1} title="Lee Adama" date="11/11/18" />
     <VolunteerCard id={1} title="Hera Agathon" date="11/11/18" />
