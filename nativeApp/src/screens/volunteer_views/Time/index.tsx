9f0ac03ff6d75bac60afd12f056bba6f530d9fca
AstroAsh <astroaisha@gmail.com>
2019-10-24 17:30:54 +0100

    💅 add CardSeparator for Time page

--- nativeApp/src/screens/volunteer_views/Time/index.tsx
+++ nativeApp/src/screens/volunteer_views/Time/index.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
-// import styled from 'styled-components/native';
 
 import TimeCard from '../../../lib/ui/TimeCard';
 import Page from '../../../lib/ui/Page';
+import CardSeparator from '../../../lib/ui/CardSeparator';
 
 /*
  * Types
@@ -19,8 +19,10 @@ type Props = {
  */
 const Time: FC<Props> = () => (
   <Page heading="Volunteer Time">
+    <CardSeparator title="Today" />
     <TimeCard id={1} timeValues={[2, 9]} labels={['General', 'Office Work']} date="30/04/19" />
     <TimeCard id={1} timeValues={[7, 19]} labels={['General', 'Office Work']} date="03/06/19" />
+    <CardSeparator title="Yesterday" />
     <TimeCard id={1} timeValues={[0, 49]} labels={['General', 'Office Work']} date="15/07/19" />
     <TimeCard id={1} timeValues={[2, 12]} labels={['General', 'Office Work']} date="30/08/19" />
     <TimeCard id={1} timeValues={[2, 18]} labels={['General', 'Office Work']} date="30/09/19" />
