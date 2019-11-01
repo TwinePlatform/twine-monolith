768cc27a55a66c7b02cfbc0bd9bd486bd92f2c3c
Elias <elias0789@gmail.com>
2019-11-01 17:40:03 +0100

    📊 Extract chart components (#380)
    related to #324 
    
    ### Changes
    - Add components for each chart on visits page
    - Extract CSV export functionality
    - Add drill-down chart component
    
    ### Testing Requirements
    N/A (manual testing will be together with subsequent PRs)
    
    ### Release Requirements
    N/A
    
    ### Manual Deployment
    N/A

---
+++ visitor-app/src/cb_admin/pages/VisitsData/TimePeriodChart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Bar } from 'react-chartjs-2';
+
+
+const TimePeriodChart = props => (
+  <Bar
+    data={props.data}
+    options={{
+      legend: { display: false },
+      scales: {
+        xAxes: [{
+          gridLines: { display: false },
+          ticks: { padding: 5 },
+        }],
+        yAxes: [{
+          gridLines: { display: false },
+          ticks: {
+            display: true,
+            beginAtZero: true,
+            stepSize: props.options.stepSize,
+          },
+        }],
+      },
+    }}
+  />
+);
+
+
+TimePeriodChart.propTypes = {
+  data: PropTypes.shape({}).isRequired,
+  options: PropTypes.shape({ stepSize: PropTypes.number }),
+};
+
+TimePeriodChart.defaultProps = {
+  options: { stepSize: 2 },
+};
+
+export default TimePeriodChart;
