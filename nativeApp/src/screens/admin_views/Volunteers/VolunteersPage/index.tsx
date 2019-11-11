4350b8b706d505802364f4e0e2230782670880e9
AstroAsh <astroaisha@gmail.com>
2019-11-11 16:00:41 +0000

    ⚛️ implement redux state in volunteers page

--- nativeApp/src/screens/admin_views/Volunteers/VolunteersPage/index.tsx
+++ nativeApp/src/screens/admin_views/Volunteers/VolunteersPage/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import moment from 'moment';
-import uuid from 'uuid/v4';
 
 import { ColorDotsLoader } from 'react-native-indicator';
 
@@ -14,6 +14,7 @@ import useRequest from '../../../../lib/hooks/requestHook';
 import { CommunityBusinesses } from '../../../../lib/api';
 import { ColoursEnum } from '../../../../lib/ui/colours';
 import { Status } from '../../../types';
+import { loadVolunteers, selectOrderedVolunteers, selectVolunteersStatus } from '../../../../redux/volunteers';
 
 /*
  * Types
@@ -29,43 +30,40 @@ type Props = {
  * Component
  */
 const Volunteers: FC<NavigationFocusInjectedProps & Props> = ({ navigation }) => {
-  const [status, setStatus] = useState(Status.loading);
-  const [deleteModalVisibility, toggleDeleteModalVisibility] = useToggle(false);
-  const [activeCard, setActiveCard] = useState(null);
-  const [reload, setReload] = useState((navigation.state.params || {}).reload);
-
-  const [volunteers] = useRequest({
-    request: CommunityBusinesses.getVolunteers,
-    updateOn: [reload],
-    setStatus,
-  });
+  // Redux
+  const dispatch = useDispatch();
 
   useEffect(() => {
-  }, [volunteers]);
+    dispatch(loadVolunteers());
+  }, []);
 
+  const volunteers = useSelector(selectOrderedVolunteers, shallowEqual);
+  const volunteersRequestStatus = useSelector(selectVolunteersStatus, shallowEqual);
+
+  // React State
+  const [deleteModalVisibility, toggleDeleteModalVisibility] = useToggle(false);
+  const [activeCard, setActiveCard] = useState(null);
+
+  // Handlers
   const onDelete = (id: number) => {
     toggleDeleteModalVisibility();
     setActiveCard(id);
   };
 
   const onConfirm = () => {
-    setStatus(Status.loading);
-    CommunityBusinesses.deleteVolunteer(activeCard)
-      .then(() => {
-        setReload(uuid());
-        toggleDeleteModalVisibility();
-      })
-      .catch((e) => {
-        setStatus(Status.failed);
-        console.log(e);
-      }); // TODO: error handling
+    /*
+    * TODO:
+    * - dispatch redux action deleteVolunteerAndUpdate(activeCard.id)
+    * - hide modal
+    * - display some type of loader whilst waiting
+    */
   };
 
-  // TODO loading spinner
-  // TODO error handling
+  // TODO: error handling
   return (
     <Page heading="Volunteers" withAddBar>
       <AddBar title="Add Volunteer" onPress={() => navigation.navigate('AdminAddVolunteer')} />
+      {/* TODO: look into Portals for potential modal replacement */}
       <ConfirmationModal
         isVisible={deleteModalVisibility}
         title="Delete"
@@ -74,7 +72,7 @@ const Volunteers: FC<NavigationFocusInjectedProps & Props> = ({ navigation }) =>
         onConfirm={onConfirm}
       />
 
-      {status === Status.loading && (
+      {volunteersRequestStatus.isFetching && (
       <ColorDotsLoader
         color1={ColoursEnum.purple}
         color2={ColoursEnum.mustard}
@@ -82,17 +80,19 @@ const Volunteers: FC<NavigationFocusInjectedProps & Props> = ({ navigation }) =>
         size={20}
       />
       )}
-      {status !== Status.loading && volunteers && volunteers.map((volunteer) => (
-        <VolunteerCard
-          key={volunteer.id}
-          id={volunteer.id}
-          title={volunteer.name}
-          date={moment(volunteer.createdAt).format('DD/MM/YY')}
-          onDelete={() => onDelete(volunteer.id)}
-          volunteerData={volunteer}
-        />
-
-      ))}
+      {/* TODO: create function for loading logic */}
+      { !volunteersRequestStatus.isFetching
+        && !volunteersRequestStatus.error
+        && volunteers.map((volunteer) => (
+          <VolunteerCard
+            key={volunteer.id}
+            id={volunteer.id}
+            title={volunteer.name}
+            date={moment(volunteer.createdAt).format('DD/MM/YY')}
+            onDelete={() => onDelete(volunteer.id)}
+            volunteerData={volunteer}
+          />
+        ))}
     </Page>
   );
 };
