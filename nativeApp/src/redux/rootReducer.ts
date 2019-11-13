24aed59b6f15e3e7f7c7bc27fdcf6f0dfa94b7e0
AstroAsh <astroaisha@gmail.com>
2019-11-13 14:16:52 +0000

    ♋️ store currentUser in redux state
    NOTE: render issues with redux dispatch/select in FC components, avoided for now

--- nativeApp/src/redux/rootReducer.ts
+++ nativeApp/src/redux/rootReducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import logsReducer from './entities/logs';
 import volunteersReducer from './entities/volunteers';
+import currentUserReducer from './currentUser';
 
 // General idea is to keep state in a "normalised" structure
 // (see https://redux.js.org/recipes/structuring-reducers/normalizing-state-shape)
 // This means keeping model data (entities) well organised and separate from other system/ui state
 export default combineReducers({
+  currentUser: currentUserReducer,
   entities: combineReducers({
     logs: logsReducer,
     volunteers: volunteersReducer,
