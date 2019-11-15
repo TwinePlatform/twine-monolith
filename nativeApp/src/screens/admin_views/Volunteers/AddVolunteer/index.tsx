3a60462c77d50b378466395ad7283ef275ea2172
AstroAsh <astroaisha@gmail.com>
2019-11-15 12:48:33 +0000

    ⚠️ wip

--- nativeApp/src/screens/admin_views/Volunteers/AddVolunteer/index.tsx
+++ nativeApp/src/screens/admin_views/Volunteers/AddVolunteer/index.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react';
 // import styled from 'styled-components/native';
+import { useDispatch } from 'react-redux';
 import Page from '../../../../lib/ui/Page';
 import UserForm from '../../../../lib/ui/forms/UserForm';
+import { createVolunteer } from '../../../../redux/entities/volunteers';
+import { NewVolunteer } from '../../../../api/types';
+import { RoleEnum } from '../../../../../../api/src/models/types';
 
 /*
  * Types
@@ -16,10 +20,20 @@ type Props = {
 /*
  * Component
  */
-const AddVolunteer: FC<Props> = () => (
-  <Page heading="Add Volunteer">
-    <UserForm onSubmit={() => {}} />
-  </Page>
-);
+const AddVolunteer: FC<Props> = () => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (data: Partial<NewVolunteer>) => {
+    console.log({ data });
+
+    const volunteer = { ...data, role: 'VOLUNTEER' as RoleEnum.VOLUNTEER }; // NB: hack to appease ts & react
+    dispatch(createVolunteer(volunteer));
+  };
+  return (
+    <Page heading="Add Volunteer">
+      <UserForm action="create" onSubmit={onSubmit} />
+    </Page>
+  );
+};
 
 export default AddVolunteer;
