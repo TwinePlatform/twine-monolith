aba3e026784abeb8b8b6c6bf2b69466308479342
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-04-23 08:11:42 +0100

    setup TnC page

--- nativeApp/src/authentication/AuthenticationLoader.tsx
+++ nativeApp/src/authentication/AuthenticationLoader.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect, FC } from 'react';
-import {
-  ActivityIndicator,
-  StatusBar,
-  View,
-} from 'react-native';
-import { NavigationInjectedProps } from 'react-navigation';
-import { useDispatch } from 'react-redux';
-import API from '../api';
-import { updateCurrentUser } from '../redux/currentUser';
+import React, { useEffect, FC } from "react";
+import { ActivityIndicator, StatusBar, View } from "react-native";
+import { NavigationInjectedProps } from "react-navigation";
+import { useDispatch } from "react-redux";
+import API from "../api";
+import { updateCurrentUser } from "../redux/currentUser";
 
 const AuthenticationLoader: FC<NavigationInjectedProps> = (props) => {
   const dispatch = useDispatch();
@@ -17,16 +13,19 @@ const AuthenticationLoader: FC<NavigationInjectedProps> = (props) => {
         const { data } = await API.Authentication.roles();
         dispatch(updateCurrentUser(data));
         // TODO get role types from api
-        if (data.roles.includes('CB_ADMIN') || data.roles.includes('VOLUNTEER_ADMIN')) {
-          props.navigation.navigate('AdminStack');
-        } else if (data.roles.includes('VOLUNTEER')) {
-          props.navigation.navigate('VolunteerStack');
+        if (
+          data.roles.includes("CB_ADMIN") ||
+          data.roles.includes("VOLUNTEER_ADMIN")
+        ) {
+          props.navigation.navigate("AdminStack");
+        } else if (data.roles.includes("VOLUNTEER")) {
+          props.navigation.navigate("VolunteerStack");
         } else {
-          throw new Error('unsupported role');
+          throw new Error("unsupported role");
         }
       } catch (error) {
         console.log({ error });
-        props.navigation.navigate('AuthStack');
+        props.navigation.navigate("AuthStack");
       }
     })();
   });
--- nativeApp/src/screens/settings_views/SettingsPage/index.tsx
+++ nativeApp/src/screens/settings_views/SettingsPage/index.tsx
@@ -1,25 +1,24 @@
-import React, { FC } from 'react';
-import styled from 'styled-components/native';
+import React, { FC } from "react";
+import styled from "styled-components/native";
 
-import { Form as F } from 'native-base';
-import { NavigationInjectedProps } from 'react-navigation';
-import { AsyncStorage } from 'react-native';
-import { Forms } from '../../../lib/ui/forms/enums';
-import LinkItem from '../../../lib/ui/forms/LinkItem';
-import Page from '../../../lib/ui/Page';
-import Toggle from '../../../lib/ui/forms/Toggle';
-import API from '../../../api';
+import { Form as F } from "native-base";
+import { NavigationInjectedProps } from "react-navigation";
+import { AsyncStorage } from "react-native";
+import { Forms } from "../../../lib/ui/forms/enums";
+import LinkItem from "../../../lib/ui/forms/LinkItem";
+import Page from "../../../lib/ui/Page";
+import Toggle from "../../../lib/ui/forms/Toggle";
+import API from "../../../api";
 /*
  * Types
  */
-type Props = {
-}
+type Props = {};
 
 /*
  * Styles
  */
 const Form = styled(F)`
-  width: ${Forms.formWidth}
+  width: ${Forms.formWidth};
 `;
 /*
  * Component
@@ -29,15 +28,21 @@ const Settings: FC<NavigationInjectedProps & Props> = ({ navigation }) => {
   const logOut = async () => {
     API.Authentication.logOut().catch(() => {});
     await AsyncStorage.clear();
-    navigation.navigate('AuthStack');
+    navigation.navigate("AuthStack");
   };
   return (
     <Page heading="Settings">
       <Form>
-        <LinkItem title="Profile" onPress={() => navigation.navigate('Profile')} />
+        <LinkItem
+          title="Profile"
+          onPress={() => navigation.navigate("Profile")}
+        />
         <LinkItem title="Help" onPress={() => {}} />
         <Toggle label="Locations reminders" />
-        <LinkItem title="Terms and Conditions" onPress={() => {}} />
+        <LinkItem
+          title="Terms & Conditions"
+          onPress={() => navigation.navigate("TnC")}
+        />
 
         <LinkItem title="Log Out" onPress={() => logOut()} />
       </Form>
