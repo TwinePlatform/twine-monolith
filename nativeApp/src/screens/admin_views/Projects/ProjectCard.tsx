7fb6f5cf3e3f64de6bdc608381b4cb5ad8c7bb52
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-05-14 13:58:43 +0100

    push notification frontend, added checkbox, text and button

--- nativeApp/src/screens/admin_views/Projects/ProjectCard.tsx
+++ nativeApp/src/screens/admin_views/Projects/ProjectCard.tsx
@@ -1,15 +1,20 @@
-import React, { FC, useState } from 'react';
-import styled from 'styled-components/native';
-import { useDispatch } from 'react-redux';
+import React, { FC, useState } from "react";
+import styled from "styled-components/native";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 
-import { MaterialIcons } from '@expo/vector-icons';
-import { withNavigation, NavigationInjectedProps } from 'react-navigation';
-import { ColoursEnum } from '../../../lib/ui/colours';
-import CardWithButtons from '../../../lib/ui/CardWithButtons';
-import { FontsEnum, Heading2 as H2 } from '../../../lib/ui/typography';
-import useToggle from '../../../lib/hooks/useToggle';
-import { ButtonConfig, ButtonType } from '../../../lib/ui/CardWithButtons/types';
-import { updateProject } from '../../../redux/entities/projects';
+import { MaterialIcons } from "@expo/vector-icons";
+import { withNavigation, NavigationInjectedProps } from "react-navigation";
+import { CheckBox } from 'react-native-elements'
+import SubmitButton from "../../../lib/ui/CardWithButtons/NotificationButton";
+import { ColoursEnum } from "../../../lib/ui/colours";
+import CardWithButtons from "../../../lib/ui/CardWithButtons";
+import { FontsEnum, Heading2 as H2 } from "../../../lib/ui/typography";
+import useToggle from "../../../lib/hooks/useToggle";
+import {
+  ButtonConfig,
+  ButtonType,
+} from "../../../lib/ui/CardWithButtons/types";
+import { updateProject, selectAllOrderedProjects } from "../../../redux/entities/projects";
 
 /*
  * Types
@@ -21,7 +26,7 @@ type Props = {
   deletedDate?: string;
   buttonType: ButtonType;
   onPress: () => void;
-}
+};
 
 /*
  * Styles
@@ -55,35 +60,39 @@ const Description = styled.Text`
 const getButtonConfig = (
   buttonType: ButtonType,
   active: boolean,
-  buttonConfigs: ButtonConfig[],
+  buttonConfigs: ButtonConfig[]
 ) => {
   switch (buttonType) {
-    case 'restore':
-      return buttonConfigs.find((x) => x.buttonType === 'restore');
-    case 'archive':
+    case "restore":
+      return buttonConfigs.find((x) => x.buttonType === "restore");
+    case "archive":
     default:
       return active
-        ? buttonConfigs.find((x) => x.buttonType === 'save')
-        : buttonConfigs.find((x) => x.buttonType === 'archive');
+        ? buttonConfigs.find((x) => x.buttonType === "save")
+        : buttonConfigs.find((x) => x.buttonType === "archive");
   }
 };
 
 const getIconColor = (buttonType: ButtonType, active: boolean) => {
   switch (buttonType) {
-    case 'restore':
+    case "restore":
       return ColoursEnum.darkGrey;
-    case 'archive':
+    case "archive":
     default:
       return active ? ColoursEnum.purple : ColoursEnum.mustard;
   }
 };
 
-
 /*
  * Component
  */
 const ProjectCard: FC<NavigationInjectedProps & Props> = ({
-  date, title, buttonType, onPress, deletedDate, id,
+  date,
+  title,
+  buttonType,
+  onPress,
+  deletedDate,
+  id,
 }) => {
   const dispatch = useDispatch();
   const [value, setValue] = useState(title);
@@ -96,29 +105,62 @@ const ProjectCard: FC<NavigationInjectedProps & Props> = ({
     toggle();
   };
 
-  const buttonConfigs: ButtonConfig[] = [{
-    buttonType: 'save',
-    onSave,
-  },
-  {
-    buttonType: 'archive',
-    onPress,
-    onEdit: toggle,
-  },
-  {
-    buttonType: 'restore',
-    onPress,
-  }];
+  const projects = useSelector(selectAllOrderedProjects, shallowEqual);
+
+
+  const buttonConfigs: ButtonConfig[] = [
+    {
+      buttonType: "save",
+      onSave,
+    },
+    {
+      buttonType: "archive",
+      onPress,
+      onEdit: toggle,
+    },
+    {
+      buttonType: "restore",
+      onPress,
+    },
+  ];
+
+  const [checked, setChecked] = useState(false);
+
+  const onSubmit = async () => {
+    console.log("I clicked the send button!");
+    console.log(projects);
+    // ToDo: use api to get all volunteers
+    // get api endpoints
+    // Knex: Select user_account_id from volunteer_hours_log WHERE volunteer_project_id = project_id;
+    // ToDo: use api to get all volunteers in project 
+  };
 
   return (
-    <CardWithButtons buttonConfig={getButtonConfig(buttonType, active, buttonConfigs)}>
+    <CardWithButtons
+      buttonConfig={getButtonConfig(buttonType, active, buttonConfigs)}
+    >
       <HeadingContainer>
         <MaterialIcons name="assignment" outline size={35} color={iconColour} />
-        {active
-          ? <InputHeading selectTextOnFocus autoFocus onChangeText={setValue} value={value} />
-          : <Heading2>{value}</Heading2>}
+        {active ? (
+          <InputHeading
+            selectTextOnFocus
+            autoFocus
+            onChangeText={setValue}
+            value={value}
+          />
+        ) : (
+            <Heading2>{value}</Heading2>
+          )}
       </HeadingContainer>
       <Description>{`Created: ${date}`}</Description>
+      <CheckBox
+        title='Send Reminder to Log Hours.'
+        checked={checked}
+        onPress={() => setChecked(currentBool => !currentBool)}
+      />
+      <SubmitButton text="Send" onPress={() => onSubmit()} />
+
+
       {deletedDate && <Description>{`Deleted: ${deletedDate}`}</Description>}
     </CardWithButtons>
   );
