4440af70876231a316215bcf2f80706266966713
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-06-10 17:18:17 +0100

    code cleaning

--- api/src/api/v1/invite/email.ts
+++ api/src/api/v1/invite/email.ts
@@ -5,9 +5,6 @@ import { Credentials as StandardCredentials } from '../../../auth/strategies/sta
 import { Api } from '../types/api';
 import { Serialisers } from '../serialisers';
 
-import * as Postmark from 'postmark';
-import { reset } from 'mockdate';
-
 
 const routes: [Api.Invite.Email.POST.Route]
   = [
@@ -37,22 +34,6 @@ const routes: [Api.Invite.Email.POST.Route]
 
         const result = await EmailService.inviteVolunteer(config, payload.To, payload.email_subject, payload.text_above_link, payload.text_below_link);
 
-        // var serverToken = "89eaa2ad-8f62-474d-9a13-7782eecdd603";
-
-        // var client = new Postmark.ServerClient(serverToken);
-
-        // const res = await client.sendEmailWithTemplate({
-        //   "From": payload.From,
-        //   "To": payload.To,
-        //   // "TemplateId": 18382605,
-        //   "templateId": EmailTemplate.WELCOME_CB_ADMIN,
-        //   "TemplateModel": {
-        //     "text_above_link": payload.TemplateModel.text_above_link,
-        //     "text_below_link": payload.TemplateModel.text_below_link
-        //   },
-        // });
-
-        // return result;
         return null;
       },
     },
