7bee0f124f7d40f56c6a410ba1b787d8a0efeba8
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-07-07 16:03:58 +0100

    removed comment on visitor route

--- api/src/api/v1/community_businesses/visitors/get.ts
+++ api/src/api/v1/community_businesses/visitors/get.ts
@@ -18,82 +18,82 @@ import { Serialisers } from '../../serialisers';
 
 
 const routes: [
-  // Api.CommunityBusinesses.Id.Visitors.GET.Route,
+  Api.CommunityBusinesses.Id.Visitors.GET.Route,
   Api.CommunityBusinesses.Me.Visitors.Id.GET.Route,
 ] = [
-    // {
-    //   method: 'GET',
-    //   path: '/community-businesses/{organisationId}/visitors',
-    //   options: {
-    //     description: 'Retreive list of all visitors from an organisation',
-    //     auth: {
-    //       strategies: ['standard', 'external'],
-    //       access: {
-    //         scope: ['user_details-child:read', 'api:visitor:read'],
-    //       },
-    //     },
-    //     validate: {
-    //       params: { organisationId: meOrId.required() },
-    //       query: {
-    //         ...query,
-    //         filter: filterQuery.filter.append({
-    //           email,
-    //           postCode,
-    //           phoneNumber,
-    //         }),
-    //         visits: Joi.boolean().default(false),
-    //       },
-    //     },
-    //     response: { schema: response },
-    //     pre: [
-    //       { method: getCommunityBusiness, assign: 'communityBusiness' },
-    //       { method: isChildOrganisation, assign: 'isChild' },
-    //     ],
-    //   },
-    //   handler: async (request, h) => {
-    //     const { query, pre: { communityBusiness, isChild }, server: { app: { knex } } } = request;
-    //     const { visits, filter } = query;
+    {
+      method: 'GET',
+      path: '/community-businesses/{organisationId}/visitors',
+      options: {
+        description: 'Retreive list of all visitors from an organisation',
+        auth: {
+          strategies: ['standard', 'external'],
+          access: {
+            scope: ['user_details-child:read', 'api:visitor:read'],
+          },
+        },
+        validate: {
+          params: { organisationId: meOrId.required() },
+          query: {
+            ...query,
+            filter: filterQuery.filter.append({
+              email,
+              postCode,
+              phoneNumber,
+            }),
+            visits: Joi.boolean().default(false),
+          },
+        },
+        response: { schema: response },
+        pre: [
+          { method: getCommunityBusiness, assign: 'communityBusiness' },
+          { method: isChildOrganisation, assign: 'isChild' },
+        ],
+      },
+      handler: async (request, h) => {
+        const { query, pre: { communityBusiness, isChild }, server: { app: { knex } } } = request;
+        const { visits, filter } = query;
 
-    //     if (request.params.organisationId !== 'me' && !isChild) {
-    //       return Boom.forbidden('Insufficient permissions to access this resource');
-    //     }
+        if (request.params.organisationId !== 'me' && !isChild) {
+          return Boom.forbidden('Insufficient permissions to access this resource');
+        }
 
-    //     const modelQuery: ModelQuery<User> = {
-    //       ...requestQueryToModelQuery<User>(query),
-    //       where: { deletedAt: null },
-    //     };
+        const modelQuery: ModelQuery<User> = {
+          ...requestQueryToModelQuery<User>(query),
+          where: { deletedAt: null },
+        };
 
-    //     // age filter
-    //     if (filter && filter.age) {
-    //       modelQuery.whereBetween = { birthYear: filter.age };
-    //     }
+        // age filter
+        if (filter && filter.age) {
+          modelQuery.whereBetween = { birthYear: filter.age };
+        }
 
-    //     modelQuery.where = mergeDeepRight(
-    //       modelQuery.where,
-    //       keys(omit(['age', 'visitActivity'], filter))
-    //         .reduce((acc, k) => assoc(k, filter[k], acc), {})
-    //     );
+        modelQuery.where = mergeDeepRight(
+          modelQuery.where,
+          keys(omit(['age', 'visitActivity'], filter))
+            .reduce((acc, k) => assoc(k, filter[k], acc), {})
+        );
 
-    //     const visitors = await (visits
-    //       ? Visitors.getWithVisits(
-    //         knex, communityBusiness, modelQuery, filter ? filter.visitActivity : undefined)
-    //       : Visitors.fromCommunityBusiness(knex, communityBusiness, modelQuery));
+        const visitors = await (visits
+          ? Visitors.getWithVisits(
+            knex, communityBusiness, modelQuery, filter ? filter.visitActivity : undefined)
+          : Visitors.fromCommunityBusiness(knex, communityBusiness, modelQuery));
 
-    //     const count = has('limit', modelQuery) || has('offset', modelQuery)
-    //       ? await Visitors.fromCommunityBusiness(
-    //         knex,
-    //         communityBusiness,
-    //         omit(['limit', 'offset'], modelQuery)
-    //       )
-    //         .then((res) => res.length)
-    //       : visitors.length;
+        const count = has('limit', modelQuery) || has('offset', modelQuery)
+          ? await Visitors.fromCommunityBusiness(
+            knex,
+            communityBusiness,
+            omit(['limit', 'offset'], modelQuery)
+          )
+            .then((res) => res.length)
+          : visitors.length;
 
-    //     return {
-    //       result: await Promise.all((visitors as User[]).map((v) => Serialisers.visitors.noSecrets(v))),
-    //       meta: { total: count },
-    //     };
-    //   },
-    // },
+        return {
+          result: await Promise.all((visitors as User[]).map((v) => Serialisers.visitors.noSecrets(v))),
+          meta: { total: count },
+        };
+      },
+    },
 
     {
       method: 'GET',
