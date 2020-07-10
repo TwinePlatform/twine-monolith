37bec65804eaa4141bd94342a0d9c25c1db90ae0
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-07-10 15:21:57 +0100

    update badges image routes

--- nativeApp/src/lib/ui/Badges/BadgesCard.tsx
+++ nativeApp/src/lib/ui/Badges/BadgesCard.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react';
 import styled from 'styled-components/native';
 
-import { Heading as H } from '../../../lib/ui/typography';
 import { ColoursEnum } from '../../../lib/ui/colours';
 
-import { Card as C, Header, Content, CardItem, Body, Text } from 'native-base';
-import { Heading2 as H2 } from '../../../lib/ui/typography';
+import { Card as C } from 'native-base';
 
 /*
  * Types
@@ -61,15 +59,15 @@ const BadgeText = styled.Text`
 
 //require the badge images 
 const BadgeImages = {
-	FirstLog: require('../../../../assets/Badges/Medal1_small.png'),
-	ThridMonth: require('../../../../assets/Badges/3MonthMedal_small.png'),
-	FifthLog: require('../../../../assets/Badges/5thLogMedal_small.png'),
-	SixthMonth: require('../../../../assets/Badges/6MonthMedal_small.png'),
-	TenthHour: require('../../../../assets/Badges/10hoursMedal_small.png'),
-	twentiethHour: require('../../../../assets/Badges/20hoursmedal_small.png'),
-	fiftiethHour: require('../../../../assets/Badges/50hoursmedal_small.png'),
-	AnnMedal: require('../../../../assets/Badges/AnnMedal_small.png'),
-	InviteMedal: require('../../../../assets/Badges/EmailInviteMedal_small.png'),
+	FirstLog: require('../../../../assets/Badges/medium/Medal1_small.png'),
+	ThridMonth: require('../../../../assets/Badges/medium/3MonthMedal_small.png'),
+	FifthLog: require('../../../../assets/Badges/medium/5thLogMedal_small.png'),
+	SixthMonth: require('../../../../assets/Badges/medium/6MonthMedal_small.png'),
+	TenthHour: require('../../../../assets/Badges/medium/10hoursMedal_small.png'),
+	twentiethHour: require('../../../../assets/Badges/medium/20hoursmedal_small.png'),
+	fiftiethHour: require('../../../../assets/Badges/medium/50hoursmedal_small.png'),
+	AnnMedal: require('../../../../assets/Badges/medium/AnnMedal_small.png'),
+	InviteMedal: require('../../../../assets/Badges/medium/EmailInviteMedal_small.png'),
 };
 
 /*
@@ -79,8 +77,6 @@ const BadgeImages = {
 export const BadgeCard = (props) => {
 	const { image, text, title } = props.badge;
 	const badgeimage = props.badge.img; //Fix: somehow destructing the image didn't work 
-	console.log('here');
-	console.log(title);
 	return (
 		<Card>
 			<BadgeIcon>
