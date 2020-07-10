c07ef932ab83310269d40d9b91f1806920536303
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-07-10 13:57:53 +0100

    create tab and use badges card

---
+++ nativeApp/src/screens/volunteer_views/VolunteerHome/BadgeObject.tsx
@@ -0,0 +1,60 @@
+export const badgearray = [
+    {
+        img: 'FirstLog',
+        title: "First Log!",
+        text: "Welcome and thank you for volunteering!"
+    },
+    {
+        img: 'ThridMonth',
+        title: "3 months!",
+        text: "Thank you for helping us, you have had an impact over the last 3 months."
+    },
+    {
+        img: 'FifthLog',
+        title: "5 logs!",
+        text: "Keep it up!"
+    },
+    {
+        img: 'SixthMonth',
+        title: "6 months!",
+        text: "Thank you for volunteering with us over the last 6 months."
+    },
+    {
+        img: 'TenthHour',
+        title: "10 hours!",
+        text: "Top 10! That's 10 hours volunteered!"
+    },
+    {
+        img: 'twentiethHour',
+        title: "20 hours!",
+        text: "Twenty's plenty! That's 20 hours volunteered."
+    },
+    {
+        img: 'fiftiethHour',
+        title: "50 hours!",
+        text: "Half century! That's 50 hours volunteered."
+    },
+    {
+        img: 'AnnMedal',
+        title: "1 year!",
+        text: "Happy anniversary!"
+    },
+    {
+        img: 'InviteMedal',
+        title: "First invitation!",
+        text: "The more the merrier!"
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nativeApp/src/screens/volunteer_views/VolunteerHome/index.tsx
+++ nativeApp/src/screens/volunteer_views/VolunteerHome/index.tsx
@@ -7,6 +7,15 @@ import { ColoursEnum } from '../../../lib/ui/colours';
 import Stat from '../../../lib/ui/Stat';
 import Line from '../../../lib/ui/Line';
 
+import Tabs from '../../../lib/ui/Tabs';
+import Page from '../../../lib/ui/Page';
+import AddBar from '../../../lib/ui/AddBar';
+
+import { BadgeCard } from '../../../lib/ui/Badges/BadgesCard';
+import useToggle from '../../../lib/hooks/useToggle';
+
+import { badgearray } from './BadgeObject';
+
 /*
  * Types
  */
@@ -26,6 +35,12 @@ const View = styled.View`
   flex: 1;
 `;
 
+const CardView = styled.View`
+  flexDirection: column;
+  alignItems: center;
+  flex: 1;
+`;
+
 const Heading = styled(H)`
   flexGrow: 0;
 `;
@@ -39,36 +54,75 @@ const Container = styled.View`
 /*
  * Component
  */
-const VolunteerHome: FC<Props> = () => (
-  <View>
-    <Heading>My Stats</Heading>
-    <Container>
-      <Stat
-        heading="TOTAL TIME GIVEN"
-        value="109"
-        unit="hours"
-      >
-        <MaterialCommunityIcons name="clock-outline" outline size={35} color={ColoursEnum.mustard} />
-      </Stat>
-      <Line />
-      <Stat
-        heading="TIMES VOLUNTEERED"
-        value="42"
-        unit="visits"
-      >
-        <MaterialCommunityIcons name="calendar-blank" outline size={35} color={ColoursEnum.mustard} />
-      </Stat>
-      <Line />
-      <Stat
-        heading="AVERAGE DURATION"
-        value="120"
-        unit="minutes"
-      >
-        <MaterialCommunityIcons name="timer" outline size={35} color={ColoursEnum.mustard} />
-      </Stat>
-    </Container>
+const Stats: FC<Props> = () => {
+  // setBadge(false);
+  return (
+    <View>
+      <Container>
+        <Stat
+          heading="TOTAL TIME GIVEN"
+          value="109"
+          unit="hours"
+        >
+          <MaterialCommunityIcons name="clock-outline" outline size={35} color={ColoursEnum.mustard} />
+        </Stat>
+        <Line />
+        <Stat
+          heading="TIMES VOLUNTEERED"
+          value="42"
+          unit="visits"
+        >
+          <MaterialCommunityIcons name="calendar-blank" outline size={35} color={ColoursEnum.mustard} />
+        </Stat>
+        <Line />
+        <Stat
+          heading="AVERAGE DURATION"
+          value="120"
+          unit="minutes"
+        >
+          <MaterialCommunityIcons name="timer" outline size={35} color={ColoursEnum.mustard} />
+        </Stat>
+      </Container>
+    </View>
+  );
+};
 
-  </View>
-);
+const BadgeTab: FC<Props> = (props) => {
+
+  return (
+    <CardView>
+      {
+        props.badge.map((element) => (
+          <BadgeCard badge={element} />
+        ))
+      }
+    </CardView >
+  );
+};
+
+// const Badges = [FirstLogCard, InviteMedalCard];
+
+const VolunteerHome: FC<Props> = () => {
+  const [stats, setBadge] = useToggle(false);
+
+
+
+  return (
+    <Page heading="Home" withAddBar>
+      <AddBar onPress={() => navigation.navigate('AdminAddProject')} title="Volunteer's Badges" />
+
+      <Tabs
+        tabOne={['Stats', Stats, {
+          // projects: projects.filter(({ deletedAt }) => !deletedAt),
+          // onConfirmDispatch: deleteProject,
+          // confirmationText: 'Are you sure you want to archive this project?',
+          // buttonType: 'archive',
+        }]}
+        tabTwo={['Badges', BadgeTab, { badge: badgearray }]}
+      />
+
+    </Page>
+  );
+}
 
 export default VolunteerHome;
