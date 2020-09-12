0b235a78abe80f2cf13aed284e1782dfe4d196b5
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-09-12 10:09:38 +0100

    merge changes (mostly dashboard) and some package changes

--- api/src/api/v1/community_businesses/get.ts
+++ api/src/api/v1/community_businesses/get.ts
@@ -13,100 +13,100 @@ const routes: [
   Api.CommunityBusinesses.Me.GET.Route,
   Api.CommunityBusinesses.Id.GET.Route
 ] = [
-  {
-    method: 'GET',
-    path: '/community-businesses',
-    options: {
-      description: 'Retrieve a list of all community businesses',
-      auth: {
-        strategy: 'standard',
-        access: {
-          scope: ['organisations_details-child:read'],
+    {
+      method: 'GET',
+      path: '/community-businesses',
+      options: {
+        description: 'Retrieve a list of all community businesses',
+        auth: {
+          strategy: 'standard',
+          access: {
+            scope: ['organisations_details-child:read'],
+          },
         },
-      },
-      validate: {
-        query,
-      },
-      pre: [requireChildOrganisation],
-      response: { schema: response },
-    },
-    handler: async (request, h) => {
-      const { server: { app: { knex } }, query } = request;
-
-      const cbs = await CommunityBusinesses.get(knex, query);
-      return Promise.all(cbs.map(Serialisers.communityBusinesses.identity));
-    },
-  },
-
-  {
-    method: 'GET',
-    path: '/community-businesses/me',
-    options: {
-      description: 'Retrieve information about users own community business',
-      auth: {
-        strategies: ['standard', 'external'],
-        access: {
-          scope: [
-            'organisations_details-own:read',
-            'organisations_details-parent:read',
-            'api:visitor:read',
-          ],
+        validate: {
+          query,
         },
+        pre: [requireChildOrganisation],
+        response: { schema: response },
       },
-      validate: {
-        query,
-      },
-      response: { schema: response },
-      pre: [
-        { method: getCommunityBusiness, assign: 'communityBusiness' },
-      ],
-    },
-    handler: async (request, h) => {
-      return Serialisers.communityBusinesses.identity(request.pre.communityBusiness);
-    },
-  },
+      handler: async (request, h) => {
+        const { server: { app: { knex } }, query } = request;
 
-  {
-    method: 'GET',
-    path: '/community-businesses/{organisationId}',
-    options: {
-      description: 'Retrieve information about users own community business',
-      auth: {
-        strategy: 'standard',
-        access: {
-          scope: ['organisations_details-child:read'],
+        const cbs = await CommunityBusinesses.get(knex, query);
+        return Promise.all(cbs.map(Serialisers.communityBusinesses.identity));
+      },
+    },
+
+    {
+      method: 'GET',
+      path: '/community-businesses/me',
+      options: {
+        description: 'Retrieve information about users own community business',
+        auth: {
+          strategies: ['standard', 'external'],
+          access: {
+            scope: [
+              'organisations_details-own:read',
+              'organisations_details-parent:read',
+              'api:visitor:read',
+            ],
+          },
         },
+        validate: {
+          query,
+        },
+        response: { schema: response },
+        pre: [
+          { method: getCommunityBusiness, assign: 'communityBusiness' },
+        ],
       },
-      validate: {
-        query,
+      handler: async (request, h) => {
+        return Serialisers.communityBusinesses.identity(request.pre.communityBusiness);
       },
-      response: { schema: response },
-      pre: [requireChildOrganisation],
     },
-    handler: async (request, h) => {
-      const {
-        params: { organisationId },
-        query: _query,
-        server: { app: { knex } } } = request;
 
-      const idQuery = (is360GivingId(organisationId))
-        ? { _360GivingId: organisationId }
-        : { id: Number(organisationId) };
+    {
+      method: 'GET',
+      path: '/community-businesses/{organisationId}',
+      options: {
+        description: 'Retrieve information about users own community business',
+        auth: {
+          strategy: 'standard',
+          access: {
+            scope: ['organisations_details-child:read'],
+          },
+        },
+        validate: {
+          query,
+        },
+        response: { schema: response },
+        pre: [requireChildOrganisation],
+      },
+      handler: async (request, h) => {
+        const {
+          params: { organisationId },
+          query: _query,
+          server: { app: { knex } } } = request;
 
-      const query = {
-        ...pick(['fields'], _query),
-        ...{ where: idQuery },
-      };
+        const idQuery = (is360GivingId(organisationId))
+          ? { _360GivingId: organisationId }
+          : { id: Number(organisationId) };
 
-      const communityBusiness = await CommunityBusinesses.getOne(knex, query);
+        const query = {
+          ...pick(['fields'], _query),
+          ...{ where: idQuery },
+        };
 
-      if (!communityBusiness) {
-        return Boom.notFound(`No community business with the id: ${organisationId}`);
-      }
+        const communityBusiness = await CommunityBusinesses.getOne(knex, query);
 
-      return Serialisers.communityBusinesses.identity(communityBusiness);
+        if (!communityBusiness) {
+          return Boom.notFound(`No community business with the id: ${organisationId}`);
+        }
+
+        return Serialisers.communityBusinesses.identity(communityBusiness);
+      },
     },
-  },
-];
+  ];
 
 export default routes;
