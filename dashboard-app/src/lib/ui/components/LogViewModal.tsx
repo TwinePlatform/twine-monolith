27888453c1638c61ca0f51fb740db1dd45765e5d
MaciekB <maciekbajda92@hotmail.com>
2020-09-24 15:39:39 +0100

    Added Create_ProjectDetails and Create_DateTime. back in for some reason they weren't there.

--- dashboard-app/src/lib/ui/components/LogViewModal.tsx
+++ dashboard-app/src/lib/ui/components/LogViewModal.tsx
@@ -104,7 +104,7 @@ const LogViewModal:FC<Props> = (props) => {
                 >
                     <p className = "modalTitle">Volunteers Time</p>
                     {/* Refactor to group both sections into 1 class */}
-                    <div className = "ProjectDetails">
+                    <div className = "Create_ProjectDetails">
                         {/* Probably need to find another wway to create spacing between letters */}
                         
                             <p><span  className = "SectionTitle">Volunteer</span> &nbsp; &nbsp; &nbsp; &nbsp;  {log.name}</p>
@@ -122,7 +122,7 @@ const LogViewModal:FC<Props> = (props) => {
                         
                     </div>
                     
-                    <div className = "DateTime">
+                    <div className = "Create_DateTime">
                         
                             {/* Discuss posiblity of  Merging  Start and Finish Date/Time  */}
                            <p><span  className = "SectionTitle">Date </span> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {log.date}</p>
