53e7eec7c24fe1eb2ff708ca51169b6d89ea93f6
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-11-21 10:31:03 +0000

    changes from the past

--- nativeApp/src/lib/ui/modals/NoteModal.tsx
+++ nativeApp/src/lib/ui/modals/NoteModal.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import Modal from 'react-native-modal';
 import {Content, Card as C, Textarea as T, Form } from 'native-base';
-import {View, KeyboardAvoidingView, Dimensions, Text, Platform} from 'react-native';
+import {View, KeyboardAvoidingView, Dimensions, Text, Platform, Keyboard} from 'react-native';
 import { AddNote, Close } from '../AddNote';
 
 import { Heading2 as H2, FontsEnum } from '../typography';
@@ -54,6 +54,21 @@ const ButtonContainer = styled.View`
   marginBottom:10;
 `;
 
+
+
+
+const getNumberOfRows = (height: number) => {
+
+  if(height < 300)
+    return 2;
+  if(height < 600)
+    return 3;
+  if(height < 1200)
+    return 4;
+
+  return 5;
+}
+
 /*
  * Component
  */
@@ -63,37 +78,74 @@ const NoteModal: FC<Props> = ({
   isVisible, addNote, initialNote, onClose
 }) => {
   const [note, setNote] = useState(initialNote);
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
 
-  return (
-    <Modal isVisible={isVisible}>
+  //tapping on the modal dismisses the keyboard
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  const modalContent = (<Card
+    onTouchEnd={keyboardVisible?Keyboard.dismiss:()=>{}}
+  >
+  <HeadingContainer>
+    <Heading2>TWINE</Heading2>
+  </HeadingContainer>
+  <Form>
+    <Textarea 
+      rowSpan={getNumberOfRows(Dimensions.get("window").height)} 
+      value={note} 
+      onChangeText={text => {setNote(text)}} 
+    />
+  </Form>
+  <View
+    style={{flexDirection: 'row-reverse'}}
+  >
+    <Text
+      style={{color: note.length>500?'red':'black', marginRight: 20}}
+    >Characters left: {500-note.length}</Text>
+  </View>
+  <ButtonContainer>
+    <Close onPress={() => { onClose(); }} />
+    <AddNote disabled={note.length>500} text='Add note' onPress={() => { addNote(note); onClose(); }} />
+    </ButtonContainer>
+  </Card>)
+
+  if(Platform.OS === "ios")
+    return (
+      <Modal isVisible={isVisible}>
+      {/* KeyboardAvoidingView blocks taps while keyboard is up
+          So, only using it where we have to on iOS.
+      */}
       <KeyboardAvoidingView
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
-        //style={{flex: 1}}
-      >
-      <Card>
-        <HeadingContainer>
-          <Heading2>TWINE</Heading2>
-        </HeadingContainer>
-        <Form>
-          <Textarea rowSpan={5} value={note} onChangeText={text => setNote(text)} 
-          //editable={note.length <= 500}
-          />
-        </Form>
-        <View
-          style={{flexDirection: 'row-reverse'}}
+        behavior={"padding"}
         >
-          <Text
-            style={{color: note.length>500?'red':'black', marginRight: 20}}
-          >Characters left: {500-note.length}</Text>
-        </View>
-        <ButtonContainer>
-          <Close onPress={() => { onClose(); }} />
-          <AddNote disabled={note.length>500} text='Add note' onPress={() => { addNote(note); onClose(); }} />
-        </ButtonContainer>
-      </Card>
+          {modalContent}
       </KeyboardAvoidingView>
-    </Modal>
-  )
+    </Modal>)
+  else
+    return (
+      <Modal isVisible={isVisible}>
+        {modalContent}
+      </Modal>
+    )
+  
 };
 
 export default NoteModal;
